d80e2b40e83489fc70ff6aa413e7c4b5a69e43aa
vereese van tonder <vereese@ska.ac.za>
2021-04-26 15:16:16 +0200

    Added summed profile

--- power_spectrum.py
+++ power_spectrum.py
@@ -1,7 +1,12 @@
 import h5py
 import numpy as np
+from constants import *
 
 vela_x = h5py.File('1604641064_wide_tied_array_channelised_voltage_0x.h5', 'r')
 vela_y = h5py.File('1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
 
 
+summed_profile = np.zeros(int(np.floor(vela_samples_T)))
+
+for i in np.arange(138):
+    summed_profile += vela_x['Data']['bf_raw'][800,i:i+74670,0]
