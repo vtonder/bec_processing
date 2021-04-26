106d16e55188964951aa62ecc41f223ace48081a
vereese van tonder <vereese@ska.ac.za>
2021-04-26 15:20:50 +0200

    Added plotting

--- power_spectrum.py
+++ power_spectrum.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
 from constants import *
+from matplotlib import pyplot as plt
 
 vela_x = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0x.h5', 'r')
 vela_y = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
@@ -10,3 +11,7 @@ summed_profile = np.zeros(int(np.floor(vela_samples_T)))
 
 for i in np.arange(138):
     summed_profile += vela_x['Data']['bf_raw'][800,i:i+74670,0]
+
+plt.figure()
+plt.plot(summed_profile)
+plt.show()
