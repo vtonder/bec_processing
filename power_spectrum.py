9b67178704b76c6cf3856755f79676b178d36b39
vereese van tonder <vereese@ska.ac.za>
2021-04-26 14:14:26 +0200

    Initial add for power_spectrum

---
+++ power_spectrum.py
@@ -0,0 +1,7 @@
+import h5py
+import numpy as np
+
+vela_x = h5py.File('1604641064_wide_tied_array_channelised_voltage_0x.h5', 'r')
+vela_y = h5py.File('1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
+
+
