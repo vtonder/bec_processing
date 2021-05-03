19281cbe6c24fb04544d3819bb5a3839302e29b7
Vereese Van Tonder <vereese@ray.ska.ac.za>
2021-05-03 09:51:13 +0200

    fold entire spectrum

--- power_spectrum.py
+++ power_spectrum.py
@@ -1,17 +1,43 @@
 import h5py
 import numpy as np
+import numba 
+import time
 from constants import *
 from matplotlib import pyplot as plt
 
 vela_x = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0x.h5', 'r')
 vela_y = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
 
+summed_profile = np.zeros([1024, int(np.floor(vela_samples_T))])
 
-summed_profile = np.zeros(int(np.floor(vela_samples_T)))
+#@numba.jit(nopython=True)
+def square(data):
+    return data*data
 
-for i in np.arange(138):
-    summed_profile += vela_x['Data']['bf_raw'][800,i:i+74670,0]
+#@numba.jit(nopython=True)
+def add(re, im):
+    return square(re) + square(im) 
 
+#@numba.jit(nopython=True)
+#def accumulate(summed_data, data, ch):
+#    summed_data[ch][:] += data
+#    return summed_data
+
+t1 = 0
+t2 = 0
+diff = 0
+for ch in np.arange(1024):
+    t1=time.time()
+    for i in np.arange(138):
+        summed_profile[ch,:] += add(vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,0], vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,1])
+    t2=time.time()
+    diff = t2-t1
+    print('at ch: ', ch, 'took ',  diff, 's')
+
+#summed_profile = sum_data(vela_x['Data']['bf_raw'][:,:,0], vela_x['Data']['bf_raw'][:,:,1])
+
+np.save('summed_profile', summed_profile)      
+print('now going to plot')
 plt.figure()
-plt.plot(summed_profile)
+plt.imshow(accumulate(summed_profile))
 plt.show()
