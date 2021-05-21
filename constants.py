4c7dc8eb291f976bd96aa41ff33905ae0a9d281c
vereese <vereese@ska.ac.za>
2021-05-21 16:24:21 +0200

    Added dedispersion
    - Code not working

--- constants.py
+++ constants.py
@@ -6,7 +6,9 @@ freq_resolution = bw / no_channels
 time_resolution = 2 * no_channels / adc_sample_rate
 
 # Pulsar information
-vela_freq = 11.195
+vela_freq = 11.1851 # 11.195
+vela_dm = 67.99 # to be applied to ms and parsec/cm^3
 vela_T = 1.0 / vela_freq  # vela period
+c = 4.148808e6 # constant for calculating
 
 vela_samples_T = vela_T / time_resolution  # samples per vela period
--- power_spectrum.py
+++ power_spectrum.py
@@ -8,9 +8,9 @@ from matplotlib import pyplot as plt
 # TODO: find out how to read a chunk and operate on entire chunks at a time
 
 PLOT = True 
-SAVE_DATA = False 
-PROCESS_DATA = False
-LOAD_PROCESSED_DATA = True
+SAVE_DATA = True 
+PROCESS_DATA = True #False 
+DEDISPERSE = True #False 
 
 #@cuda.jit#nopython=True)
 #def square(data, y):
@@ -26,15 +26,24 @@ def add(re, im, x):
 #def accumulate(summed_data, data, ch):
 #    summed_data[ch][:] += data
 #    return summed_data
+font = {'family' : 'STIXGeneral',
+        'size'   : 22}
+
+        #'weight' : 'bold',
+        
+plt.rc('font', **font)
 
 
 if PROCESS_DATA:
+    #vela_x = h5py.File('/home/vereese/pulsar_data/1604641569_wide_tied_array_channelised_voltage_0x.h5', 'r')
+    #vela_x = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
     vela_x = h5py.File('/home/vereese/pulsar_data/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r')
-    
+
     num_data_points = vela_x['Data']['timestamps'].shape[0]
+    print(num_data_points)
     num_pulses = int(np.floor(num_data_points/vela_samples_T)) #number of vela pulses per observation
     vela_samples_T = int(np.floor(vela_samples_T))
-    fp = 2 #number of vela periods to fold over
+    fp = 1 #number of vela periods to fold over
     
     print("read in data")
     
@@ -55,7 +64,19 @@ if PROCESS_DATA:
     print("getting im ", t)
     im = all_data[:,:,1].astype('int16')#vela_x['Data']['bf_raw'].value[:,:,1]
     
+    #for i in np.arange(100,(int(num_pulses/fp))-100):
+    frequencies = np.arange(1712+(freq_resolution/1e6)/2,856+(freq_resolution/1e6)/2,-freq_resolution/1e6)
+    f2 = 1712+((freq_resolution/1e6)/2)
+    if DEDISPERSE:
+        for i, freq in enumerate(frequencies):
+            delay = c*vela_dm*(1/(f2**2) - 1/(freq**2))
+            num_2_roll = int(np.round(delay/(time_resolution*1000)))
+            print(freq, delay, num_2_roll)
+            re[i,:] = np.roll(re[i,:], num_2_roll)
+            im[i,:] = np.roll(im[i,:], num_2_roll)
+
     for i in np.arange(int(num_pulses/fp)):
+
         t1=time.time()
         print(t1)
     
@@ -75,18 +96,32 @@ if PROCESS_DATA:
         #summed_profile[i,:] = summed_profile[i,:]-mean
         inverted_summed_profile[(no_channels-1)-i,:] = summed_profile[i,:]-mean
 
+    #frequencies = np.arange(856+(freq_resolution/1e6)/2,1712+(freq_resolution/1e6)/2,-freq_resolution/1e6)
+    #f2 = 1284.0+((freq_resolution/1e6)/2)
+    #if DEDISPERSE:
+    #    for i, freq in enumerate(frequencies):
+    #        delay = c*vela_dm*(1/(freq**2) - 1/(f2**2))
+    #        num_2_roll = int(np.round(delay/(time_resolution*1000)))
+    #        print(freq, delay, num_2_roll)
+    #        inverted_summed_profile[i,:] = np.roll(inverted_summed_profile[i,:], num_2_roll)
+    #        #re[i,:] = np.roll(re[i,:], num_2_roll)
+    #        #im[i,:] = np.roll(im[i,:], num_2_roll)
+
+
     if SAVE_DATA:
-        np.save('summed_profile2', inverted_summed_profile)      
+        np.save('summed_profile', inverted_summed_profile)      
 
 if PLOT: 
-    summed_profile = np.load('summed_profile2.npy')
+    summed_profile = np.load('summed_profile.npy')
     plt.figure()
     plt.autoscale(True)
-    #plt.imshow(summed_profile, aspect='auto', extent=[0,2,856,1712])#, origin='lower')
+    #plt.imshow(summed_profile, aspect='auto', extent=[0,1,856,1712])#, origin='lower')
     #plt.imshow(summed_profile, aspect='auto')
-    plt.ylabel('frequency [MHz]')
-    plt.xlabel('pulsar phase')
-    plt.imshow(np.roll(summed_profile, 40000, axis=1), aspect='auto', extent=[0,2,856,1712]) #interpolation='nearest'
+    plt.ylabel('Frequency [MHz]')
+    plt.xlabel('Pulsar phase')
+    plt.imshow(np.roll(summed_profile, -30000, axis=1), aspect='auto', extent=[0,4,856,1712]) #interpolation='nearest'
+    #plt.colorbar()
+    #plt.clim([-0.5,7000])
     #plt.plot(summed_profile[100,:])
     plt.show()
 
