144f3d3a7daaf4177a388d5a31623419bc0ffebb
vereese <vereese@ska.ac.za>
2021-06-04 10:33:42 +0200

    Added time series calculation

--- constants.py
+++ constants.py
@@ -4,9 +4,16 @@ bw = 856e6
 no_channels = 1024
 freq_resolution = bw / no_channels
 time_resolution = 2 * no_channels / adc_sample_rate
+#(1284e6-856e6)/freq_resolution - to get frequency bin
 
 # Pulsar information
-vela_freq = 11.1851 # 11.195
+vela_freq = 11.185 #11.18512 #11.195 # 
+#vela_freq = 11.184893175250126
+#vela_freq = 11.184168797246667
+#tot_obs=(vela_samples_T*time_resolution*22*11) 22 vela pulses , 22 is chosen randomly, that gave 11 subintegrations
+#deltaT=(time_resolution*17500) # 17500 is a rough estimate from the plot
+# vela_true_period = vela_T*(1+delaT/tot_obs)
+
 vela_dm = 67.99 # to be applied to ms and parsec/cm^3
 vela_T = 1.0 / vela_freq  # vela period
 c = 4.148808e6 # constant for calculating
--- power_spectrum.py
+++ power_spectrum.py
@@ -6,11 +6,14 @@ from constants import *
 from matplotlib import pyplot as plt
 
 # TODO: find out how to read a chunk and operate on entire chunks at a time
+# https://stackoverflow.com/questions/21766145/h5py-correct-way-to-slice-array-datasets
 
 PLOT = True 
 SAVE_DATA = True 
-PROCESS_DATA = True #False 
-DEDISPERSE = True #False 
+PROCESS_DATA = True 
+COMPUTE_POWER_SPECTA = True 
+COMPUTE_TIME_SERIES = True 
+DEDISPERSE = True 
 
 #@cuda.jit#nopython=True)
 #def square(data, y):
@@ -35,93 +38,129 @@ plt.rc('font', **font)
 
 
 if PROCESS_DATA:
-    #vela_x = h5py.File('/home/vereese/pulsar_data/1604641569_wide_tied_array_channelised_voltage_0x.h5', 'r')
+    vela_x = h5py.File('/home/vereese/pulsar_data/1604641569_wide_tied_array_channelised_voltage_0x.h5', 'r') #driver core makes it really slow here
     #vela_x = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
-    vela_x = h5py.File('/home/vereese/pulsar_data/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r')
+    #vela_x = h5py.File('/home/vereese/pulsar_data/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r', driver="core")
 
-    num_data_points = vela_x['Data']['timestamps'].shape[0]
-    print(num_data_points)
+    num_data_points = vela_x['Data/timestamps'].shape[0]
+    print("Number of data points", num_data_points)
     num_pulses = int(np.floor(num_data_points/vela_samples_T)) #number of vela pulses per observation
     vela_samples_T = int(np.floor(vela_samples_T))
     fp = 1 #number of vela periods to fold over
     
-    print("read in data")
-    
     summed_profile = np.zeros([no_channels, fp*vela_samples_T])
     inverted_summed_profile = np.zeros([no_channels, fp*vela_samples_T])
 
-    #temp = np.zeros([no_channels, fp*vela_samples_T])
-    
     t=time.time()
-    print("reading all: ", t)
-    all_data = vela_x['Data']['bf_raw'][()] #.value[:,:,0]
-    
+    print("read in data t:", t)
+    data = vela_x['Data/bf_raw'][...]
     t=time.time()
-    print("getting re ", t)
-    re = all_data[:,:,0].astype('int16')
+    print("done reading in data: ", t)
+
+    #all_data = vela_x['Data']['bf_raw'][()] #.value[:,:,0]
+    #re = all_data[:,:,0].astype('int16')
+    #im = all_data[:,:,1].astype('int16')#vela_x['Data']['bf_raw'].value[:,:,1]
     
-    t=time.time()
-    print("getting im ", t)
-    im = all_data[:,:,1].astype('int16')#vela_x['Data']['bf_raw'].value[:,:,1]
-    
-    #for i in np.arange(100,(int(num_pulses/fp))-100):
-    frequencies = np.arange(1712+(freq_resolution/1e6)/2,856+(freq_resolution/1e6)/2,-freq_resolution/1e6)
+    frequencies = np.arange(856+(freq_resolution/1e6)/2,1712+(freq_resolution/1e6)/2,freq_resolution/1e6)
     f2 = 1712+((freq_resolution/1e6)/2)
     if DEDISPERSE:
         for i, freq in enumerate(frequencies):
             delay = c*vela_dm*(1/(f2**2) - 1/(freq**2))
             num_2_roll = int(np.round(delay/(time_resolution*1000)))
             print(freq, delay, num_2_roll)
-            re[i,:] = np.roll(re[i,:], num_2_roll)
-            im[i,:] = np.roll(im[i,:], num_2_roll)
+            data[i,:,0] = np.roll(data[i,:,0], num_2_roll) # re[i,:] = np.roll(re[i,:], num_2_roll)
+            data[i,:,1] = np.roll(data[i,:,1], num_2_roll)
 
-    for i in np.arange(int(num_pulses/fp)):
+    if COMPUTE_TIME_SERIES:
+        # randomly chose to integrate 22 vela pulses per sub-integration
+        num_sub_ints = int(200) # number of sub-integrations
+        step = int(vela_samples_T * num_sub_ints)
+        num_int = int(np.floor(num_data_points/step)) # total number of integrations
+        vela_sub_int = np.zeros([num_int,vela_samples_T])
+        fc = 512 
+        for i in np.arange(num_int):
+            print("at integration ", i, " of ", num_int)
+            for j in np.arange(num_sub_ints):
+                start = (i*step)+(j*vela_samples_T)
+                stop = (i*step)+((j+1)*vela_samples_T)
+                re = data[fc, start:stop, 0].astype(np.float)
+                im = data[fc, start:stop, 1].astype(np.float)
+                vela_sub_int[i,:] += re**2 + im**2
 
-        t1=time.time()
-        print(t1)
-    
-        #add(vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,0], vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,1], temp)
-        summed_profile += re[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2 + im[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2
-    
-        t2=time.time()
-        diff = t2-t1
-        print('at addition: ', i, 'took ',  diff, 's')
-    
-    # take the mean and subtract from each channel to rid the RFI
-    # TODO: look into using max power, then sigma, then mean statistics to get rid of RFI
-    # invert the channels because we are working with filterbank data
-    # In filterbank data the ch0 corresponds to higher frequency components and the higher channels correspond to lower frequencies
-    for i in np.arange(no_channels):
-        mean = np.mean(summed_profile[i,:])
-        #summed_profile[i,:] = summed_profile[i,:]-mean
-        inverted_summed_profile[(no_channels-1)-i,:] = summed_profile[i,:]-mean
+        if SAVE_DATA:
+            np.save('sub_integration_true_period_1569', vela_sub_int)      
+            
+        if PLOT:
+            plt.figure()
+            plt.autoscale(True)
+            plt.ylabel('sub integrations')
+            plt.xlabel('Pulsar phase')
+            plt.imshow(vela_sub_int, aspect='auto')
+            plt.show()
 
-    #frequencies = np.arange(856+(freq_resolution/1e6)/2,1712+(freq_resolution/1e6)/2,-freq_resolution/1e6)
-    #f2 = 1284.0+((freq_resolution/1e6)/2)
-    #if DEDISPERSE:
-    #    for i, freq in enumerate(frequencies):
-    #        delay = c*vela_dm*(1/(freq**2) - 1/(f2**2))
-    #        num_2_roll = int(np.round(delay/(time_resolution*1000)))
-    #        print(freq, delay, num_2_roll)
-    #        inverted_summed_profile[i,:] = np.roll(inverted_summed_profile[i,:], num_2_roll)
-    #        #re[i,:] = np.roll(re[i,:], num_2_roll)
-    #        #im[i,:] = np.roll(im[i,:], num_2_roll)
+    if COMPUTE_POWER_SPECTA:
+        tot_int = int(num_pulses/fp)
+        rem = 0
+        for i in np.arange(tot_int):
+            t1=time.time()
+            print(t1)
+        
+            #add(vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,0], vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,1], temp)
+            #summed_profile += re[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2 + im[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2
+            #vela_x['Data/bf_raw'].read_direct(data, source_sel=np.s_[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),:])
+            #vela_x['Data']['bf_raw'].read_direct(im, source_sel=np.s_[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),1])
+            rem += 
+            re = data[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),0].astype(np.float)
+            im = data[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),1].astype(np.float)
+
+            summed_profile += re**2 + im**2
+
+        
+            t2=time.time()
+            diff = t2-t1
+            print('at addition: ', i, 'of', tot_int ,'took ',  diff, 's')
+        
+        # take the mean and subtract from each channel to rid the RFI
+        # TODO: look into using max power, then sigma, then mean statistics to get rid of RFI
+        # invert the channels because we are working with filterbank data
+        # In filterbank data the ch0 corresponds to higher frequency components and the higher channels correspond to lower frequencies
+        for i in np.arange(no_channels):
+            #mean = np.mean(summed_profile[i,:])
+            #summed_profile[i,:] = summed_profile[i,:]-mean
+            inverted_summed_profile[(no_channels-1)-i,:] = summed_profile[i,:]#-mean
 
 
-    if SAVE_DATA:
-        np.save('summed_profile', inverted_summed_profile)      
+        if SAVE_DATA:
+            np.save('summed_profile_1569', inverted_summed_profile)      
 
 if PLOT: 
-    summed_profile = np.load('summed_profile.npy')
-    plt.figure()
-    plt.autoscale(True)
-    #plt.imshow(summed_profile, aspect='auto', extent=[0,1,856,1712])#, origin='lower')
+    summed_profile = np.load('summed_profile_1569.npy')
+    for i in np.arange(no_channels):
+        mean = np.mean(summed_profile[i,:])
+        summed_profile[i,:] = summed_profile[i,:]-mean
+
+    plt.figure(0)
+    #plt.autoscale(True)
+    plt.imshow(summed_profile, aspect='auto', extent=[0,1,856,1712])#, origin='lower')
     #plt.imshow(summed_profile, aspect='auto')
+    #plt.plot(summed_profile[292,:])
     plt.ylabel('Frequency [MHz]')
     plt.xlabel('Pulsar phase')
-    plt.imshow(np.roll(summed_profile, -30000, axis=1), aspect='auto', extent=[0,4,856,1712]) #interpolation='nearest'
+    #plt.imshow(np.roll(summed_profile, -30000, axis=1), aspect='auto', extent=[0,1,856,1712]) #interpolation='nearest'
     #plt.colorbar()
     #plt.clim([-0.5,7000])
     #plt.plot(summed_profile[100,:])
     plt.show()
 
+    vela_sub_int = np.load('sub_integration_true_period_1569.npy')
+    print(np.shape(vela_sub_int[:,0:2500]))
+    print(np.shape(vela_sub_int))
+
+
+
+    plt.figure(1)
+    plt.autoscale(True)
+    plt.ylabel('sub integrations')
+    plt.xlabel('Pulsar phase')
+    plt.imshow(vela_sub_int[:,10000:25000], aspect='auto', origin='lower')
+    plt.show()
