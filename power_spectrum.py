3550f0b4d1a99f6d88cfd4415bcf4cb16e176419
vereese van tonder <vereese@ska.ac.za>
2021-07-24 14:16:50 +0200

    Initial addition of coherent dedispersion algorithm

--- power_spectrum.py
+++ power_spectrum.py
@@ -14,6 +14,7 @@ PROCESS_DATA = True
 COMPUTE_POWER_SPECTA = True 
 COMPUTE_TIME_SERIES = True 
 DEDISPERSE = True 
+CODEDISPERSE = True
 
 #@cuda.jit#nopython=True)
 #def square(data, y):
@@ -70,6 +71,12 @@ if PROCESS_DATA:
             print(freq, delay, num_2_roll)
             data[i,:,0] = np.roll(data[i,:,0], num_2_roll) # re[i,:] = np.roll(re[i,:], num_2_roll)
             data[i,:,1] = np.roll(data[i,:,1], num_2_roll)
+
+    if CODEDISPERSE:
+        k = -2*np.pi*freq_resolution**2*vela_dm/(2.41*10**-4)
+        for i, freq in enumerate(frequencies):
+            ism = np.exp(k/(freq**2*(freq_resolution+freq)))
+            data[i,:,:] = data[i,:,:] * ism
 
     if COMPUTE_TIME_SERIES:
         # randomly chose to integrate 22 vela pulses per sub-integration
