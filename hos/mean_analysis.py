966f37ab9e9b2efa19c11752355083563c56a87a
vereese van tonder <vereese@ska.ac.za>
2022-08-09 11:52:37 +0200

    Major refactor + parameterisation

--- hos/mean_analysis.py
+++ hos/mean_analysis.py
@@ -4,80 +4,117 @@ from hos import Bispectrum
 import time
 import sys
 sys.path.append('..')
-from constants import *
-from matplotlib import pyplot as plt
+from constants import start_indices, num_ch
+import argparse
 
-# TODO: Parameterise CAPITAL and N, freq ch values
-# TODO: Finish up plot on time vs mean and std err of mean as error bar (this will be for only 1 channel)
-# TODO: Create a plot of freq ch vs mean of means and std err of mean as error bar
 
 # Assume the distribution of the majority of data to be 0 mean Gaussian.
 # This script uses the 3 sigma normality test in order to detect outliers
 # Answers: what % of outliers are in the data set
-# Creates a histogram of means  
 # Calculate the mean of means and standards deviation of means. ie get mean of 10000 samples , then next 
 # Ref: https://www.middleprofessor.com/files/applied-biostatistics_bookdown/_book/variability-and-uncertainty-standard-deviations-standard-errors-confidence-intervals.html
 # Script to show that the std deviation of set of means is the std error as calculated using std / sqrt(N)
 # where std is the sample standard deviation and N is the number of samples in that set
 
-DIRECTORY = '/home/vereese/phd_data/'
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="observation file to process")
+parser.add_argument("N", type=int, help="number of samples in a set")
+parser.add_argument("-s", type=int, help="number of sets. If not given calculate from entire data set.")
+parser.add_argument("-c", "--ch", type=int, dest="ch", help="frequency channel to analyse. "
+                                                            "If given number of channels is set to 1. "
+                                                            "If omitted, analyse all channels")
+parser.add_argument("-d", "--directory", dest="directory", help="path of directory to save data products to",
+                    default="/home/vereese/phd_data/")
+parser.add_argument("-e", "--std_error", dest="std_error", help="Compute the standard error of the mean estimate of each sample")
+parser.add_argument("-m", "--median", dest="median", help="Compute the median of each set")
+parser.add_argument("-v", "--variance", dest="var", help="Compute the variance of each set")
+parser.add_argument("-o", "--outlier_detection", dest="outlier", help="Conduct 3 sigma outlier test")
+args = parser.parse_args()
+
 OUTLIER_TEST = False
-PLOT_DATA = False 
-SAVE_DATA = False 
 
-"""for i, arg in enumerate(sys.argv, start = 1):
-    if arg == '-h':
-        print("Argument 1: Frequency channel, default set to 856")
-        print("Argument 2: Number of samples in a set")
-
-try:
-  opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-except getopt.GetoptError:
-  print 'test.py -i <inputfile> -o <outputfile>'
-  sys.exit(2)
-for opt, arg in opts:
-  if opt == '-h':
-     print 'test.py -i <inputfile> -o <outputfile>'
-     sys.exit()
-  elif opt in ("-i", "--ifile"):
-     inputfile = arg
-  elif opt in ("-o", "--ofile"):
-     outputfile = arg
-print 'Input file is "', inputfile
-print 'Output file is "', outputfile"""
-
-#vela_y = h5py.File('/home/vereese/pulsar_data/1604641064_wide_tied_array_channelised_voltage_0y.h5', 'r')
-vela_y = h5py.File('/home/vereese/pulsar_data/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r')
-data = vela_y['Data/bf_raw'][...]
-data_re = data[:,13634560:,0]
-data_im = data[:,13634560:,1]
+data_file = h5py.File('/home/vereese/pulsar_data/'+args.file, 'r')
+data = data_file['Data/bf_raw'][...]
+start_index = start_indices[args.file]
+data_re = data[:, start_index:, 0]
+data_im = data[:, start_index:, 1]
 data_len = len(data_re[0,:]) # Re & Im freq channels will have the same length
-num_ch = 1024 # number of frequency channels
-N = 10000 # number of samples in a set
-S = int(data_len/N) # number of sets
+
+N = args.N # number of samples in a set
+# number of sets
+if args.s:
+    S = args.s
+else:
+    S = int(data_len/N)
+
+if args.ch:
+    num_ch = 1
+
 means_re, means_im = np.zeros([num_ch, S]), np.zeros([num_ch, S])
 std_err_re, std_err_im = np.zeros([num_ch, S]), np.zeros([num_ch, S])
+median_re, median_im = np.zeros([num_ch, S]), np.zeros([num_ch, S])
+var_re, var_im = np.zeros([num_ch, S]), np.zeros([num_ch, S])
+outlier_re, outlier_im = [[] for _ in np.arange(num_ch)], [[] for _ in np.arange(num_ch)]
 
-print("number of sets:", S)
-print("shape re", np.shape(data_re))
-print("shape im", np.shape(data_im))
 
-for k in np.arange(1024):
-    for i in np.arange(S):
-        print("ch:",k," set num:",i)
-        print("start:", i*N, " end:",(i+1)*N)
-        means_re[k,i] = np.mean(data_re[k, i*N:(i+1)*N])
-        means_im[k,i] = np.mean(data_im[k, i*N:(i+1)*N])
-        std_err_re[k,i] = np.sqrt(np.var(data_re[k, i*N:(i+1)*N])/N)
-        std_err_im[k,i] = np.sqrt(np.var(data_im[k, i*N:(i+1)*N])/N)
+print("total data length            : ", data_len)
+print("number of frequency channels : ", num_ch)
+print("number of samples in a set   : ", N)
+print("number of sets               : ", S)
 
-np.save("means_re", means_re)
-np.save("means_im", means_im)
-np.save("std_err_re", std_err_re)
-np.save("std_err_im", std_err_im)
+t1 = time.time()
+for i in np.arange(S):
+    re_part = data_re[:, i*N:(i+1)*N]
+    im_part = data_im[:, i*N:(i+1)*N]
+    means_re[:,i] = np.mean(re_part, axis=1)
+    means_im[:,i] = np.mean(im_part, axis=1)
+    if args.std_error:
+        std_err_re[:,i] = np.sqrt(np.var(re_part, axis=1)/N)
+        std_err_im[:,i] = np.sqrt(np.var(im_part, axis=1)/N)
+    if args.median:
+        median_re[:, i] = np.median(re_part, axis=1)
+        median_im[:, i] = np.median(im_part, axis=1)
+print("Analysis took: ", time.time()-t1, " s")
 
-# These were only for 1 channel
+#TODO: Compute outliers based on MAD rejection
+# https://casper.astro.berkeley.edu/wiki/Impulsive_RFI_Excision:_CASPER_Library_Block
+if args.outlier:
+    t1 = time.time()
+    mean_re = np.mean(data_re, axis=1)
+    mean_im = np.mean(data_im, axis=1)
+    var_re = np.var(data_re, axis=1)
+    var_im = np.var(data_im, axis=1)
+    std_re = np.sqrt(var_re)
+    std_im = np.sqrt(var_im)
 
+    up_re = mean_re + 3*(std_re)
+    up_im = mean_im + 3*(std_im)
+    lo_re = mean_re - 3*(std_re)
+    lo_im = mean_im - 3*(std_im)
+
+    for i in np.arange(num_ch):
+        [outlier_re[i].append(1) for val in data_re[i,:] if val < lo_re[i] or up_re[i] < val]
+        [outlier_im[i].append(1) for val in data_im[i,:] if val < lo_im[i] or up_im[i] < val]
+
+    # % outliers per frequency channel
+    perc_re_outliers = [(sum(ch_data)/data_len)*100 for ch_data in outlier_re]
+    perc_im_outliers = [(sum(ch_data)/data_len)*100 for ch_data in outlier_im]
+
+    print("Outlier detection took: ", time.time()-t1, " s")
+
+    np.save(args.directory+args.file[6:10]+'_real_outliers', perc_re_outliers)
+    np.save(args.directory+args.file[6:10]+'_imag_outliers', perc_im_outliers)
+
+np.save(args.directory+args.file[6:10]+"_means_re_"+str(args.N)+"_"+str(S), means_re)
+np.save(args.directory+args.file[6:10]+"_means_im"+str(args.N)+"_"+str(S), means_im)
+if args.std_error:
+    np.save(args.directory+args.file[6:10]+"_std_err_re"+str(args.N)+"_"+str(S), std_err_re)
+    np.save(args.directory+args.file[6:10]+"_std_err_im"+str(args.N)+"_"+str(S), std_err_im)
+if args.median:
+    np.save(args.directory+args.file[6:10]+"_median_re"+str(args.N)+"_"+str(S), std_err_re)
+    np.save(args.directory+args.file[6:10]+"_median_im"+str(args.N)+"_"+str(S), std_err_im)
+
+# TODO: delete this code.
 """print("var of sample", np.var(data_re[:,N:2*N]))
 print("std of sample", np.sqrt(np.var(data_re[:,N:2*N])))
 print("")
@@ -89,12 +126,6 @@ print("var of means im", np.var(means_im))
 print("std dev of means re", np.sqrt(np.var(means_re)))
 print("std dev of means im", np.sqrt(np.var(means_im)))
 print("")
-
-print("std error of means re", np.sqrt(np.var(data_re[0:N])/N))
-print("std error of means im", np.sqrt(np.var(data_im[0:N])/N))
-print("std error of means re", np.sqrt(np.var(data_re[N:2*N])/N))
-print("std error of means im", np.sqrt(np.var(data_im[N:2*N])/N))
-print("")"""
 
 if OUTLIER_TEST:
     mean_re = np.mean(data_re)
@@ -128,35 +159,5 @@ if OUTLIER_TEST:
     print("% real outliers: ", re_outliers)
     print("% imag outliers: ", im_outliers)
 
-    if SAVE_DATA:
-        np.save(DIRECTORY+'real_outliers', re_outliers)
-        np.save(DIRECTORY+'imag_outliers', im_outliers)
-
-if PLOT_DATA:
-    plt.figure(0)
-    plt.hist(means_re)
-    plt.title("Histogram means real data")
-    plt.grid()
-
-    plt.figure(1)
-    plt.hist(means_im)
-    plt.title("Histogram means imag data")
-    plt.grid()
-
-    plt.figure(2)
-    plt.plot(means_re, label='mean')
-    plt.plot(std_err_re,'o', label='std err')
-    plt.plot(-1*std_err_re,'o', label='std err')
-    plt.title("means + std re data")
-    plt.legend()
-    plt.grid()
-
-    plt.figure(3)
-    plt.plot(means_im, label='mean')
-    plt.plot(std_err_im, 'o',label='std err')
-    plt.plot(-1*std_err_im, 'o',label='std err')
-    plt.title("means + std imag data")
-    plt.legend()
-    plt.grid()
-    plt.show()
-
+    np.save(args.directory+'_real_outliers', re_outliers)
+    np.save(args.directory+'_imag_outliers', im_outliers)"""
