921b02a19cfb94eb844c8a1bcadf6053bc81cc6a
vereese van tonder <vereese@ska.ac.za>
2022-08-23 15:48:28 +0200

    Retest folding wrt refactoring

--- constants.py
+++ constants.py
@@ -9,8 +9,8 @@ import numpy as np
 adc_sample_rate = 1712
 bw = 856
 num_ch = 1024
-freq_resolution = bw / num_ch
-time_resolution = 2 * num_ch / adc_sample_rate
+freq_resolution = bw / num_ch # MHz
+time_resolution = 2 * num_ch / adc_sample_rate # us
 
 # Chunking
 # looks like for dedispersion would need to share 14 chunks for reading from different locations
@@ -20,7 +20,7 @@ freq_chunk_size = 1024
 time_chunk_size = 16384
 re_im_chunk_size = 2
 #(1284e6-856e6)/freq_resolution - to get frequency bin
-dispersion_constant = 4.148808e3 # constant for calculating dispersion delay TODO double check if this is correct?
+dispersion_constant = 4.148808e3 # constant for calculating dispersion delay as applied to ms TODO double check if this is correct?
 
 # Popular frequencies in MHz
 h1 = 1420.4
@@ -48,23 +48,22 @@ start_indices = {
 
 # Pulsar information
 # Vela
-vela_freq = 11.185084597305504  #11.185053620637202  #11.185031494489326 #11.18500936838522
-#vela_freq = 11.1946499395 #11.184900190310714 #11.18512 #11.18512 #11.195 # 
+vela_freq = 11.185084597305504  # unit Hz 11.185053620637202  #11.185031494489326 #11.18500936838522
+#vela_freq = 11.1946499395 #11.184900190310714 #11.18512 #11.18512 #11.195 # 11.1946499395
 #vela_freq = 11.184893175250126
 #vela_freq = 11.184168797246667
 #tot_obs=(vela_samples_T*time_resolution*22*11) 22 vela pulses , 22 is chosen randomly, that gave 11 subintegrations
 #deltaT=(time_resolution*17500) # 17500 is a rough estimate from the plot
 # vela_true_period = vela_T*(1+delaT/tot_obs)
 
-vela_dm = 67.99 # to be applied to ms and parsec/cm^3
+vela_dm = 67.97 # from ATNF catalog. units: parsec/cm^3
 # vela_dm = 68.0247344970703 <-> where is this from?
-vela_T = 1.0 / vela_freq  # vela period
-
-vela_samples_T = vela_T / time_resolution  # samples per vela period
+vela_T = 1.0 / vela_freq  # vela period unit s
+vela_samples_T = vela_T*10**6 / time_resolution  # samples per vela period
 
 # J0742-2822 
 J0742_T = 166.762*10**-3
 J0742_freq = 1/J0742_T
 J0742_dm = 73.78
-J0742_samples_T = J0742_T / time_resolution
+J0742_samples_T = J0742_T*10**6 / time_resolution
 
--- pulsar.py
+++ pulsar.py
@@ -1,5 +1,5 @@
 import h5py
-from constants import frequencies, freq_resolution, time_resolution, num_ch, dispersion_constant
+from constants import frequencies, freq_resolution, time_resolution, num_ch, dispersion_constant, vela_T, vela_dm
 import time
 import numpy as np
 
@@ -9,26 +9,24 @@ import numpy as np
 
 class Pulsar:
 
-    def __init__(self, period, dm, file_name, fp):
-        self.period = period  # is
-        self.frequency = 1 / period  # Hz
-        self.dm = dm  # to be applied to ms and parsec/cm^3
+    def __init__(self, rotational_frequency, dm, file_name, fp=1):
+        self.rotational_frequency = rotational_frequency  # Hz
+        self.period = 1 / self.rotational_frequency  # s
+        self.dm = dm  # parsec/cm^3
         self.data = h5py.File(file_name, 'r')
-        self.fp = fp  # number of periods to fold over
-        self.samples_T = period / time_resolution  # number of samples per period
+        self.fp = fp  # number of periods to fold over ie fold in multiples of 1 or 2 periods, default is 1
+        self.samples_T = self.period*10**6 / time_resolution  # number of samples per period
         self.samples_int_T = int(self.samples_T)  # integer number of samples per period
         self.num_data_points = self.data['Data/timestamps'].shape[0]
         self.num_pulses = np.floor(self.num_data_points / self.samples_T)
         self.folded_profile = np.zeros([num_ch, fp * self.samples_int_T])
-        self.inverted_folded = np.zeros([num_ch, fp * self.samples_int_T])
 
     def incoherent_dedisperse(self):
-        f2 = 1712 + (freq_resolution / 2)
+        f2 = 1712 - (freq_resolution / 2)
         for i, freq in enumerate(frequencies):
-            # delay = c*vela_dm*(1/(f2**2) - 1/(freq**2))
-            delay = dispersion_constant * self.dm * (1 / (f2 ** 2) - 1 / (freq ** 2))
 
-            num_2_roll = int(np.round(delay / (time_resolution * 1000)))
+            delay = 10**6*(dispersion_constant * self.dm * (1/f2**2 - 1/freq**2)) # us
+            num_2_roll = int(np.round(delay/time_resolution)) # samples
             print(freq, delay, num_2_roll)
             # re[i,:] = np.roll(re[i,:], num_2_roll)
             # im[i,:] = np.roll(im[i,:], num_2_roll)
@@ -60,41 +58,31 @@ class Pulsar:
             np.save('sub_int_', J0742_sub_int)
 
     def frequency_phase_power_spectrum(self):
-        tot_int = int(self.num_pulses / self.fp)
-        for i in np.arange(tot_int):
-            t1 = time.time()
-            print(t1)
-
-            # add(vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,0], vela_x['Data']['bf_raw'][ch,i*74670:(i+1)*74670,1], temp)
-            # summed_profile += re[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2 + im[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T)]**2
-            # vela_x['Data/bf_raw'].read_direct(data, source_sel=np.s_[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),:])
-            # vela_x['Data']['bf_raw'].read_direct(im, source_sel=np.s_[:,i*(fp*vela_samples_T):(i+1)*(fp*vela_samples_T),1])
-
-            # start = int(i*(fp * vela_samples_T))
-            # end = start + vela_int_samples_T
-
+        total_integrations = int(self.num_pulses / self.fp)
+        for i in np.arange(total_integrations):
             start = int(i * (self.fp * self.samples_T))
             end = int(start + (self.fp * self.samples_int_T))
-
-            # end = int((i + 1) * (fp * vela_samples_T))
 
             if end >= self.num_data_points:
                 break
 
             re = self.data[:, start:end, 0].astype(np.float)
             im = self.data[:, start:end, 1].astype(np.float)
+            self.folded_profile += re**2 + im**2
 
-            # summed_profile += re[:,start:end]**2 + im[:,start:end]**2
-            self.folded_profile += re ** 2 + im ** 2
-            t2 = time.time()
-            diff = t2 - t1
-            print('at addition: ', i, 'of', tot_int, 'took ', diff, 's')
 
+    def rfi_mitigation(self):
         # take the mean and subtract from each channel to rid the RFI
         # TODO: look into using max power, then sigma, then mean statistics to get rid of RFI
-        # invert the channels because we are working with filterbank data
-        # In filterbank data the ch0 corresponds to higher frequency components and the higher channels correspond to lower frequencies
         for i in np.arange(num_ch):
             mean = np.mean(self.folded_profile[i, :])
             self.folded_profile[i, :] = self.folded_profile[i, :] - mean
-            self.inverted_folded[(num_ch - 1) - i, :] = self.folded_profile[i, :] - mean
+
+if __name__== '__main__':
+    pulsar = Pulsar(vela_T, vela_dm, "/net/com08/data6/vereese/1604641569_wide_tied_array_channelised_voltage_0x.h5")
+    pulsar.incoherent_dedisperse()
+    t1 = time.time()
+    pulsar.frequency_phase_power_spectrum()
+    print("Folding took: {0} s".format(time.time()-t1))
+
+    np.save('/home/vereese/phd_data/pulsar/1569/dedispersed_folder_power_spectrum',pulsar.folded_profile)
