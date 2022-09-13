7cde484d6bf850439315f037f56f6473eb88793f
vereese van tonder <vereese@ska.ac.za>
2022-09-13 10:23:10 +0200

    Read re im at same time to get 2 times speed up in reading

--- hos/meerkat_hos.py
+++ hos/meerkat_hos.py
@@ -14,20 +14,16 @@ if ANALYSE:
     t1 = time.time()
     vela_y = h5py.File('/net/com08/data6/vereese/1604641569_wide_tied_array_channelised_voltage_0y.h5', 'r')
     #data = vela_y['Data/bf_raw'][...]
-    data_re_h1 = np.transpose(vela_y['Data/bf_raw'][h1_ch,13625088:,0])
-    data_im_h1 = np.transpose(vela_y['Data/bf_raw'][h1_ch,13625088:,1])
-    data_re_l1 = np.transpose(vela_y['Data/bf_raw'][gps_l1_ch,13625088:,0])
-    data_im_l1 = np.transpose(vela_y['Data/bf_raw'][gps_l1_ch,13625088:,1])
-    data_re_l2 = np.transpose(vela_y['Data/bf_raw'][gps_l2_ch,13625088:,0])
-    data_im_l2 = np.transpose(vela_y['Data/bf_raw'][gps_l2_ch,13625088:,1])
-    data_re_g6 = np.transpose(vela_y['Data/bf_raw'][gal_e6_ch,13625088:,0])
-    data_im_g6 = np.transpose(vela_y['Data/bf_raw'][gal_e6_ch,13625088:,1])
+    data_h1 = np.transpose(vela_y['Data/bf_raw'][h1_ch,13625088:,:].astype(np.float))
+    data_l1 = np.transpose(vela_y['Data/bf_raw'][gps_l1_ch,13625088:,:].astype(np.float))
+    data_l2 = np.transpose(vela_y['Data/bf_raw'][gps_l2_ch,13625088:,:].astype(np.float))
+    data_g6 = np.transpose(vela_y['Data/bf_raw'][gal_e6_ch,13625088:,:].astype(np.float))
     print("reading in all channels took: {0} s".format(time.time()-t1))
 
-    b_h1 = Bispectrum(data_re_h1+1j*data_im_h1, reshape=True, fft_size=1024, method='direct')
-    b_gps_l1 = Bispectrum(data_re_l1+1j*data_im_l1, reshape=True, fft_size=1024, method='direct')
-    b_gps_l2 = Bispectrum(data_re_l2+1j*data_im_l2, reshape=True, fft_size=1024, method='direct')
-    b_gal_e6 = Bispectrum(data_re_g6+1j*data_im_g6, reshape=True, fft_size=1024, method='direct')
+    b_h1 = Bispectrum(data_h1[:,0]+1j*data_h1[:,1], reshape=True, fft_size=1024, method='direct')
+    b_gps_l1 = Bispectrum(data_l1[:,0]+1j*data_l1[:,1], reshape=True, fft_size=1024, method='direct')
+    b_gps_l2 = Bispectrum(data_l2[:,0]+1j*data_l2[:,1], reshape=True, fft_size=1024, method='direct')
+    b_gal_e6 = Bispectrum(data_g6[:,0]+1j*data_g6[:,1], reshape=True, fft_size=1024, method='direct')
     #b.mean_compensation()
     #b.calc_power_spectrum()
     t1 = time.time()
@@ -43,12 +39,15 @@ if ANALYSE:
 
 if PLOT:
     DIR='/home/vereese/git/phd_data/meerkat_hos/'
-    data = {'gps_l1_bispec.npy':[], 'gps_l2_bispec.npy': [], 'gal_e6_bispec.npy': [], 'h1_bispec.npy':[], 'vela_bispec.npy':[]}
+    data = {'gps_l1_bispec.npy':[], 'gps_l2_bispec.npy': [], 'gal_e6_bispec.npy': [], 'gal_5b_bispec_0y.npy': [],
+            'h1_bispec_0y.npy':[], 'vela_bispec.npy':[], 'clean_bispec.npy':[], 'dirty_bispec.npy':[], 'clean2_bispec_0y.npy':[], 'dirty2_bispec_0y.npy':[]}
+    #data = {'h1_bispec.npy':[]}
 
     for i,fn in enumerate(data.keys()):
         data[fn] = np.load(DIR+fn)
         plt.figure(i)
         plt.imshow(np.abs(data[fn]), aspect='auto', origin='lower')
+        #plt.imshow(np.angle(data[fn]), aspect='auto', origin='lower')
         plt.title(fn)
 
     plt.show()
