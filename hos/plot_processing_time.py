1129681e01d78752c11812eb5864049952e91588
vereese van tonder <vereese@ska.ac.za>
2022-09-13 10:17:55 +0200

    Mod to plot scripts

--- hos/plot_processing_time.py
+++ hos/plot_processing_time.py
@@ -1,22 +1,28 @@
 from matplotlib import pyplot as plt
+
+font = {'family': 'STIXGeneral',
+        'size': 26}
+plt.rc('font', **font)
+
 processing_time ={
-  '5':2951.9122191639617,##/2919.0,
-  '10':1477.788330548443,#/1459.0,
-  '15':1003.9506793580949,#/973.0,
-  '20':759.8343786997721,#/729.0,
-  '25':626.7423422709107,#/583.0,
-  '30':541.3413198925555,#/486.0,
-  '35':482.35602218285203,#/417.0,
-  '40':593.6757857976481,#/364.0
-  '45':1745.7610048977658 #/364.0
+  '5': 3034.3588042259216,
+  '10':1574.2632755279542,
+  '15':1047.1665592193604,
+  '20':789.898957490921,
+  '25':709.3646445274353,
+  '30':672.2443092823029,
+  '35':643.1528521537781,
+  '40':576.8024142742157,
+  '45':537.296351480484
 }
 
 num_proc = [int(i) for i in processing_time.keys()]
 
-plt.figure(0)
+plt.figure(0, figsize=[15,12])
 plt.plot(num_proc, list(processing_time.values()))
 plt.xlim(num_proc[0], num_proc[-1])
 plt.grid()
-plt.xlabel("number of processors")
-plt.ylabel("processing time per chunk")
+plt.xlabel("Number of processors")
+plt.ylabel("Total processing time (s)")
+plt.savefig('/home/vereese/Documents/PhD/CASPER2022/presentation/proctime', bbox_inches='tight')
 plt.show()
