15c4af24e3abf469e75182b184ad3c9bc50dd41e
vereese van tonder <vereese@ska.ac.za>
2022-12-14 10:09:09 +0200

    Added normalisation factor

--- hos/kurtosis.py
+++ hos/kurtosis.py
@@ -1,9 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import firwin, lfilter
 
-def spectral_kurtosis(s, M, FFT_LEN):
+#TODO need to apply normalisation to spectra otherwise the estimator will fail - Nita 2007
+def spectral_kurtosis(s, M, FFT_LEN, fft = True, normalise=True):
+    N = FFT_LEN * M
+    if fft:
+        perio = np.abs(np.fft.fft(s.reshape(M, FFT_LEN), axis=1)) ** 2 / FFT_LEN
+    else:
+        perio = s.reshape(M, FFT_LEN) # FFT has already been taken
 
-    perio = np.abs(np.fft.fft(s.reshape(M, FFT_LEN), axis=1)) ** 2 / FFT_LEN
+    if normalise:
+        NTAPS = 8
+        PFB_M = NTAPS*FFT_LEN
+        pfb_window = np.hamming(PFB_M) * np.sinc((np.arange(PFB_M) - PFB_M / 2.0) / FFT_LEN)
+        perio = 2*perio/(N*np.sum(pfb_window**2))
 
     S1 = perio.sum(axis=0)
     S2 = np.sum(perio ** 2, axis=0)
@@ -19,18 +30,30 @@ if __name__ == "__main__":
     M = 6000 # number of averages to take PSD over
     N = FFT_LEN * M
 
-    f1 = 10
+    f1 = 20
     fs = 100
     t = np.arange(0, N/fs, 1.0/fs)
+    k = np.arange(FFT_LEN/2)
+    f = k/FFT_LEN*fs
     s = np.sin(2*np.pi*f1*t)
     wgn = np.random.normal(mean, std, size=N)
     x = s + wgn
+    coeffs = firwin(128, [0.05, 0.25], width=0.05, pass_zero=False)
+    h_even = coeffs[::2]
+    h_odd = coeffs[1::2]
 
-    SK = spectral_kurtosis(x, M, FFT_LEN)
+    y_even = lfilter(h_even, 1, x)
+    y_odd = lfilter(h_odd, 1, x)
+
+    # Reconstruct the filtered signal
+    y = y_even + y_odd
+
+
+    SK = spectral_kurtosis(y, M, FFT_LEN)
 
     plt.figure(0)
     plt.axhline(1.6)
     plt.axhline(0.4)
-    plt.plot(SK)
+    plt.plot(f, SK[0:int(FFT_LEN/2)])
     plt.grid()
     plt.show()
--- hos/meerkat_sk.py
+++ hos/meerkat_sk.py
@@ -4,19 +4,25 @@ from kurtosis import spectral_kurtosis
 import time
 import sys
 sys.path.append('..')
-from constants import h1_ch, gps_l1_ch, gps_l2_ch, gal_e6_ch
+from constants import h1_ch, gps_l1_ch, gps_l2_ch, gal_e6_ch, time_resolution
 from matplotlib import pyplot as plt
 
 vela_y = h5py.File('/net/com08/data6/vereese/1604641569_wide_tied_array_channelised_voltage_0y.h5', 'r')
-data_h1 = vela_y['Data/bf_raw'][h1_ch,13625088:,:].astype(np.float)
+ch = h1_ch
+data = vela_y['Data/bf_raw'][ch, 13625088:, :].astype(np.float)
+
 FFT_LEN = 1024
-M = np.floor(len(data_h1[0]) / FFT_LEN)
+M = np.floor(len(data[0]) / FFT_LEN)
 N = FFT_LEN*M
-SK = spectral_kurtosis(data_h1[0:N, 0]+1j*data_h1[0:N, 1], M, FFT_LEN)
+fs = 1/time_resolution
+k = np.arange(int(FFT_LEN/2))
+freqs = k/FFT_LEN * fs
+
+SK = spectral_kurtosis(data[0:N, 0] + 1j * data[0:N, 1], M, FFT_LEN)
 
 plt.figure(0)
 plt.axhline(1.6)
 plt.axhline(0.4)
-plt.plot(SK)
+plt.plot(freqs, SK[0:int(FFT_LEN/2)])
 plt.grid()
 plt.show()
