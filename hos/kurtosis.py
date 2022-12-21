7bfbaee3102b07dd82a9e6dc59413f13a20bfdb3
vereese van tonder <vereese@ska.ac.za>
2022-12-21 14:51:40 +0200

    Added a column major SK implementation

--- hos/kurtosis.py
+++ hos/kurtosis.py
@@ -1,34 +1,94 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import firwin, lfilter
+from scipy.fftpack import fft
+import numpy as np
+
+def gpt_spectral_kurtosis(x, fs):
+    """
+    Calculate the spectral kurtosis of a signal.
+
+    Parameters:
+    x (ndarray): The input signal.
+    fs (float): The sampling frequency of the signal.
+
+    Returns:
+    ndarray: The spectral kurtosis of the input signal at each frequency bin.
+    """
+    # Calculate the Fourier transform of the signal
+
+    Pxx = np.abs(fft(np.asarray(x), 1024))
+    M=5000
+    # Calculate the spectral kurtosis for each frequency bin
+    #SK = ((M + 1) / (M - 1)) * ((M * S2 / S1 ** 2) - 1)
+    #sk = np.mean(Pxx ** 4, axis=0) / np.mean(Pxx ** 2, axis=0) ** 2
+    sk = ((M + 1) / (M - 1)) * (((Pxx ** 4) / (Pxx ** 2) ** 2) - 1)
+    print("GPT SK", sk)
+    return sk
+
 
 #TODO need to apply normalisation to spectra otherwise the estimator will fail - Nita 2007
-def spectral_kurtosis(s, M, FFT_LEN, fft = True, normalise=True):
-    N = FFT_LEN * M
-    if fft:
-        perio = np.abs(np.fft.fft(s.reshape(M, FFT_LEN), axis=1)) ** 2 / FFT_LEN
-    else:
-        perio = s.reshape(M, FFT_LEN) # FFT has already been taken
+#row major implementation
+def spectral_kurtosis(s, M, FFT_LEN, N = 1, d = 1, reshape= True, fft = True, normalise=True):
 
-    if normalise:
-        NTAPS = 8
-        PFB_M = NTAPS*FFT_LEN
-        pfb_window = np.hamming(PFB_M) * np.sinc((np.arange(PFB_M) - PFB_M / 2.0) / FFT_LEN)
-        perio = 2*perio/(N*np.sum(pfb_window**2))
+    if reshape:
+        s = s.reshape(M, FFT_LEN)
+
+    if fft:
+        perio = np.abs(np.fft.fft(s, axis=1)) ** 2 / FFT_LEN
+    else:
+        perio = np.abs(s)**2 / FFT_LEN # FFT has already been taken
+
+    #if normalise:
+        #NTAPS = 8
+        #PFB_M = NTAPS*FFT_LEN
+        #wipfb_ndow = np.hamming(PFB_M) * np.sinc((np.arange(PFB_M) - PFB_M / 2.0) / FFT_LEN)
+        #x = np.ones([M*FFT_LEN, FFT_LEN])
+        #xf = np.zeros([M, FFT_LEN])
+        #for i in np.arange(M):
+        #    if (i + 8 == M - 1):
+        #        break
+        #    xf[i, :] = np.sum(np.multiply(x[i:i + 8, :], pfb_window), axis=0)
+        #xf = np.sum(xf, axis=0)
+
+        #perio = (2/(FFT_LEN*np.sum(pfb_window**2)))*perio
 
     S1 = perio.sum(axis=0)
     S2 = np.sum(perio ** 2, axis=0)
 
-    SK = ((M + 1) / (M - 1)) * ((M * S2 / S1 ** 2) - 1)
+    SK = ((M*N*d + 1) / (M - 1)) * ((M * S2 / S1 ** 2) - 1)
+
+    if normalise:
+        pfb_window = np.hamming(FFT_LEN) * np.sinc((np.arange(FFT_LEN) - FFT_LEN / 2.0) / FFT_LEN)
+        n = np.arange(FFT_LEN)
+        k = np.arange(FFT_LEN)
+        weights = np.ones(FFT_LEN)
+        for i in k:
+            for j in n:
+                weights[i] += pfb_window[j]**2 * np.exp(-4*np.pi*i*j/FFT_LEN)
+            weights[i] = weights[i]/np.sum(pfb_window**2)
+        print("weights: ", np.abs(weights)**2)
+        SK = SK-np.abs(weights)**2
+
+    return SK
+
+#column major SK implementation for use with filterbank data
+def spectral_kurtosis_cm(s, M, FFT_LEN, N = 1, d = 1):
+    perio = np.abs(s) ** 2 / FFT_LEN  # FFT has already been taken
+
+    S1 = perio.sum(axis=1)
+    S2 = np.sum(perio ** 2, axis=1)
+
+    SK = ((M*N*d + 1) / (M - 1)) * ((M * S2 / S1 ** 2) - 1)
 
     return SK
 
 if __name__ == "__main__":
     mean = 0
     std = 2
-    FFT_LEN = 2048
-    M = 6000 # number of averages to take PSD over
+    FFT_LEN = 1024
+    M = 128 # number of averages to take PSD over
     N = FFT_LEN * M
+    PFB_TAPS = 8
+    PFB_M = PFB_TAPS*FFT_LEN
 
     f1 = 20
     fs = 100
@@ -36,24 +96,67 @@ if __name__ == "__main__":
     k = np.arange(FFT_LEN/2)
     f = k/FFT_LEN*fs
     s = np.sin(2*np.pi*f1*t)
+
+    pulse_train = np.zeros(N)
+    pulse_train[2432000:2688000] = 50 #np.ones(256000)*50
+    #for i in np.arange(0,N,10*FFT_LEN):
+    #    pulse_train[i+100:i+450] = np.ones(350)*50
+
     wgn = np.random.normal(mean, std, size=N)
-    x = s + wgn
-    coeffs = firwin(128, [0.05, 0.25], width=0.05, pass_zero=False)
-    h_even = coeffs[::2]
-    h_odd = coeffs[1::2]
+    x =  wgn #+pulse_train
+    xsk = gpt_spectral_kurtosis(x,1024)
 
-    y_even = lfilter(h_even, 1, x)
-    y_odd = lfilter(h_odd, 1, x)
+    pfb_window = np.hamming(PFB_M) * np.sinc((np.arange(PFB_M) - PFB_M / 2.0) / FFT_LEN)
+    pfb_window = pfb_window.reshape(PFB_TAPS, FFT_LEN)
+    x = x.reshape(M, FFT_LEN)
+    xf = np.zeros([M, FFT_LEN])
+    print("X shape: ", np.shape(x))
+    for i in np.arange(M):
+        if (i+8 == M-1):
+            break
+        xf[i,:] = np.sum(np.multiply(x[i:i+8,:], pfb_window), axis=0)
 
-    # Reconstruct the filtered signal
-    y = y_even + y_odd
+    xf = xf[0:M-8,:]
+    XF = np.fft.fft(x, axis=1)
+    XPF = np.fft.fft(xf, axis=1)
 
+    PXF = np.abs(XF**2 / FFT_LEN).sum(axis=0)
+    PXPF = np.abs(XPF**2 / FFT_LEN).sum(axis=0)
 
-    SK = spectral_kurtosis(y, M, FFT_LEN)
+    #SK1 = spectral_kurtosis(XF, M, FFT_LEN, reshape=False, fft=False, normalise=False)
+    SK2 = spectral_kurtosis(XPF, M, FFT_LEN, reshape=False, fft=False, normalise=False)
+    SK3 = spectral_kurtosis(XPF, M, FFT_LEN, reshape=False, fft=False, normalise=True)
+
+    #print("SK 1 mean:", np.mean(SK1))
+    print("SK 2 mean:", np.mean(SK2))
+    #print("SK 3 mean:", np.mean(SK3))
 
     plt.figure(0)
+    plt.plot(x.reshape(N,1))
+    plt.grid()
+
+    plt.figure(1)
+    plt.plot(pfb_window.reshape(PFB_M))
+    plt.grid()
+
+    plt.figure(2)
+    plt.plot(f, PXF[0:int(FFT_LEN/2)], label='FFT')
+    plt.plot(f, PXPF[0:int(FFT_LEN/2)], label='PFB')
+    plt.grid()
+    plt.legend()
+
+    plt.figure(3)
     plt.axhline(1.6)
     plt.axhline(0.4)
-    plt.plot(f, SK[0:int(FFT_LEN/2)])
+    #plt.plot(f, SK1[0:int(FFT_LEN/2)], label='FFT No Norm')
+    plt.plot(f, SK2[0:int(FFT_LEN/2)], label='PFB No Norm')
+    plt.plot(f, SK3[0:int(FFT_LEN/2)], label='PFB Norm')
+    plt.legend()
     plt.grid()
+
+    plt.figure(4)
+    plt.plot(xsk)
+    plt.grid()
+
     plt.show()
+
--- hos/meerkat_sk.py
+++ hos/meerkat_sk.py
@@ -1,24 +1,30 @@
 import h5py
 import numpy as np
-from kurtosis import spectral_kurtosis
+from kurtosis import spectral_kurtosis_cm
 import time
 import sys
+from mpi4py import MPI
 sys.path.append('..')
+from constants import start_indices, num_ch, time_chunk_size
 from constants import h1_ch, gps_l1_ch, gps_l2_ch, gal_e6_ch, time_resolution
 from matplotlib import pyplot as plt
 
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
 vela_y = h5py.File('/net/com08/data6/vereese/1604641569_wide_tied_array_channelised_voltage_0y.h5', 'r')
 ch = h1_ch
-data = vela_y['Data/bf_raw'][ch, 13625088:, :].astype(np.float)
+data = vela_y['Data/bf_raw'][:, 831*time_chunk_size:832*time_chunk_size, :].astype(np.float)
 
 FFT_LEN = 1024
-M = np.floor(len(data[0]) / FFT_LEN)
-N = FFT_LEN*M
+#M = np.floor(len(data[0]) / FFT_LEN)
+#N = FFT_LEN*M
 fs = 1/time_resolution
 k = np.arange(int(FFT_LEN/2))
 freqs = k/FFT_LEN * fs
 
-SK = spectral_kurtosis(data[0:N, 0] + 1j * data[0:N, 1], M, FFT_LEN)
+SK = spectral_kurtosis_cm(data[:, :, 0] + 1j * data[:, :, 1], time_chunk_size, FFT_LEN)
 
 plt.figure(0)
 plt.axhline(1.6)
