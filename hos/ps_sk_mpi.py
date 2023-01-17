5c36f1381e4dfc762dc513e064d4aff2a8f89b2a
vereese <vereese@ska.ac.za>
2023-01-17 20:55:06 +0200

    a

--- hos/ps_sk_mpi.py
+++ hos/ps_sk_mpi.py
@@ -25,21 +25,24 @@ M = 512
 num_data_points = df['Data/timestamps'].shape[0] - start_index
 num_sk = int(num_data_points/M/size)
 
+print("num_sk: ", num_sk)
+
 start = start_index + rank*num_sk*M
 stop = start + num_sk*M
-local_data = data[:, start:stop, :].astype(np.float)/128  # get the portion of the array to be analyzed by each rank
+#print(start, stop)
+#local_data = data[:, start:stop, :].astype(np.float)  # get the portion of the array to be analyzed by each rank
 
 # SK RFI mitigation
 FFT_LEN = int(1024)
-low_lim = 0.776424
-up_lim = 1.32275
+low_lim = 0.77511
+up_lim = 1.3254
 SK = np.zeros([FFT_LEN, num_sk])
 SK_flags = np.zeros([FFT_LEN, num_sk])
 
 for i,idx in enumerate(np.arange(start, stop, M)):
-    SK[:, i] = spectral_kurtosis_cm(local_data[:, idx:idx+M, 0] + 1j*local_data[:, idx:idx+M, 1], M, FFT_LEN)
+    SK[:, i] = spectral_kurtosis_cm(data[:, idx:idx+M, 0] + 1j*data[:, idx:idx+M, 1], M, FFT_LEN)
     for j, val in enumerate(SK[:, i]):
-        if val < low_lim :
+        if val < low_lim or val > up_lim:
             SK_flags[j, i] = 1
 
 # send results to rank 0
@@ -47,8 +50,8 @@ if rank > 0:
     comm.Send([SK_flags, MPI.DOUBLE], dest=0, tag=14)  # send results to process 0
     comm.Send([SK, MPI.DOUBLE], dest=0, tag=15)  # send results to process 0
 else:
-    tot_SK = np.zeros([FFT_LEN, num_sk])
-    tot_SK_flags = np.zeros([FFT_LEN, num_sk])
+    tot_SK = np.zeros([FFT_LEN, size*num_sk])
+    tot_SK_flags = np.zeros([FFT_LEN, size*num_sk])
     tot_SK[:,0:num_sk] = SK
     tot_SK_flags[:,0:num_sk] = SK_flags
 
@@ -60,7 +63,7 @@ else:
         tot_SK[:,int(i*num_sk):int((i+1)*num_sk)] = tmp_SK
         tot_SK_flags[:,int(i*num_sk):int((i+1)*num_sk)] = tmp_SK_flags
 
-    np.save('mpi_sk_flags_M519_1234_0x', tot_SK_flags)
-    np.save('mpi_sk_M519_1234_0x', tot_SK)
+    np.save('mpi_sk_flags_M512_1234_0x', tot_SK_flags)
+    np.save('mpi_sk_M512_1234_0x', tot_SK)
     print("procesing took: ", time.time() - t1)
 
