12a4f74a07f10d396a251b5cd0b75c70802ce86c
vereese <vereese@ska.ac.za>
2023-01-18 12:02:04 +0200

    b

--- hos/ps_sk_mpi.py
+++ hos/ps_sk_mpi.py
@@ -17,7 +17,7 @@ rank = comm.Get_rank()
 if rank == 0:
     t1 = time.time()
 
-df = h5py.File('/net/com08/data6/vereese/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r', rdcc_nbytes=0)
+df = h5py.File('/net/com08/data6/vereese/1604641234_wide_tied_array_channelised_voltage_0x.h5', 'r')
 data = df['Data/bf_raw']
 start_index = start_indices['1604641234_wide_tied_array_channelised_voltage_0x.h5']
 
@@ -37,33 +37,33 @@ FFT_LEN = int(1024)
 low_lim = 0.77511
 up_lim = 1.3254
 SK = np.zeros([FFT_LEN, num_sk])
-SK_flags = np.zeros([FFT_LEN, num_sk])
+#SK_flags = np.zeros([FFT_LEN, num_sk])
 
 for i,idx in enumerate(np.arange(start, stop, M)):
-    SK[:, i] = spectral_kurtosis_cm(data[:, idx:idx+M, 0] + 1j*data[:, idx:idx+M, 1], M, FFT_LEN)
-    for j, val in enumerate(SK[:, i]):
-        if val < low_lim or val > up_lim:
-            SK_flags[j, i] = 1
+    SK[:, i] = spectral_kurtosis_cm(data[:, idx:idx+M, 0].astype(float)/128 + 1j*data[:, idx:idx+M, 1].astype(float)/128, M, FFT_LEN)
+    #for j, val in enumerate(SK[:, i]):
+    #    if val < low_lim or val > up_lim:
+    #        SK_flags[j, i] = 1
 
 # send results to rank 0
 if rank > 0:
-    comm.Send([SK_flags, MPI.DOUBLE], dest=0, tag=14)  # send results to process 0
+    #comm.Send([SK_flags, MPI.DOUBLE], dest=0, tag=14)  # send results to process 0
     comm.Send([SK, MPI.DOUBLE], dest=0, tag=15)  # send results to process 0
 else:
     tot_SK = np.zeros([FFT_LEN, size*num_sk])
-    tot_SK_flags = np.zeros([FFT_LEN, size*num_sk])
+    #tot_SK_flags = np.zeros([FFT_LEN, size*num_sk])
     tot_SK[:,0:num_sk] = SK
-    tot_SK_flags[:,0:num_sk] = SK_flags
+    #tot_SK_flags[:,0:num_sk] = SK_flags
 
     for i in range(1, size):  # determine the size of the array to be received from each process
         tmp_SK = np.zeros([FFT_LEN, num_sk])
-        tmp_SK_flags = np.zeros([FFT_LEN, num_sk])
-        comm.Recv([tmp_SK_flags, MPI.DOUBLE], source=i, tag=14)  # receive results from the process
+        #tmp_SK_flags = np.zeros([FFT_LEN, num_sk])
+        #comm.Recv([tmp_SK_flags, MPI.DOUBLE], source=i, tag=14)  # receive results from the process
         comm.Recv([tmp_SK, MPI.DOUBLE], source=i, tag=15)  # receive SK results from the process
         tot_SK[:,int(i*num_sk):int((i+1)*num_sk)] = tmp_SK
-        tot_SK_flags[:,int(i*num_sk):int((i+1)*num_sk)] = tmp_SK_flags
+        #tot_SK_flags[:,int(i*num_sk):int((i+1)*num_sk)] = tmp_SK_flags
 
-    np.save('mpi_sk_flags_M512_1234_0x', tot_SK_flags)
+    #np.save('mpi_sk_flags_M512_1234_0x', tot_SK_flags)
     np.save('mpi_sk_M512_1234_0x', tot_SK)
     print("procesing took: ", time.time() - t1)
 
