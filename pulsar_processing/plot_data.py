c8da1eb7f36cca5f5c76c6419a469da3bdaaccbc
vereese <vereese@ska.ac.za>
2023-03-03 12:19:27 +0200

    Update required because moved script into separate directory

--- pulsar_processing/plot_data.py
+++ pulsar_processing/plot_data.py
@@ -1,9 +1,11 @@
 import numpy as np
+import sys
+sys.path.append('../')
 from constants import *
 from matplotlib import pyplot as plt
 
 
-summed_profile = np.load('/home/vereese/git/phd_data/mpi_res.npy')
+summed_profile = np.load('/home/vereese/pulsar_processing/hos/mpi_ps.npy')
 #for i in np.arange(no_channels):
 #    mean = np.mean(summed_profile[i, :])
 #    summed_profile[i, :] = summed_profile[i, :] - mean
@@ -13,12 +15,16 @@ plt.figure(0)
 plt.imshow(summed_profile, aspect='auto', extent=[0, 1, 0, 1024] , origin='lower')
 # plt.imshow(summed_profile, aspect='auto')
 # plt.plot(summed_profile[292,:])
-plt.ylabel('Frequency [MHz]')
-plt.xlabel('Pulsar phase')
+#plt.ylabel('Frequency [MHz]')
+#plt.xlabel('Pulsar phase')
 # plt.imshow(np.roll(summed_profile, -30000, axis=1), aspect='auto', extent=[0,1,856,1712]) #interpolation='nearest'
 # plt.colorbar()
 # plt.clim([-0.5,7000])![](../../Documents/PhD/URSI2022/rfi.png)
 # plt.plot(summed_profile[100,:])
+
+plt.figure(1)
+plt.plot(summed_profile[305, :])
+plt.grid()
 plt.show()
 
 '''vela_sub_int = np.load('sub_integration_true_period_1569.npy')
