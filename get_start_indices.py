ca2ae37684d579f39b4f889966fb9fda5e67d3d8
vereese van tonder <vereese@ska.ac.za>
2023-03-16 15:48:35 +0200

    Update to respect the chunks

--- get_start_indices.py
+++ get_start_indices.py
@@ -2,29 +2,36 @@ import sys
 import h5py
 import numpy as np
 import glob
+from constants import freq_chunk_size, time_chunk_size
+import yaml
+import time
 
 # The meerkat data has lots of 0s
 # This script takes h5py input file
 # It calculates the first index to be non 0 per channel
 # The ultimate index chosen is the maximum index between all the channels for re and imaginary
 
-file_names = glob.glob('/home/vereese/pulsar_data/*.h5')
+file_names = glob.glob('/net/com08/data6/vereese/*.h5')
+start_indices = {}
 for file_name in file_names:
+    t1 = time.time()
     input_data = h5py.File(file_name, 'r')
-    data = input_data['Data/bf_raw'][...]
-    data_re = data[:,:,0]
-    data_im = data[:,:,1]
-    print("data len re: ", len(data_re)) # Re & Im freq channels will have the same length
-    print("data len im: ", len(data_im))
-    re_start, im_start = [], []
+    data = input_data['Data/bf_raw']
+    dl = data.shape[1]
+    num_chunk = dl/time_chunk_size
+    start_index = 0
+    for i in np.arange(num_chunk):
+        start = i*time_chunk_size
+        stop = start + time_chunk_size
+        dt = data[:,start:stop,:]
+        for ch in np.arange(freq_chunk_size):
+            temp_non_zero = next(i for i, x in enumerate(data[ch, :, :]) if any(x))
+            if start_index < temp_non_zero:
+                start_index = temp_non_zero
 
-    # Throw away 55 ch on each side given receiver band is 900-1670 MHz
-    for k in np.arange(55,969):
-        print(k)
-        re_start.append(next(i for i,j in enumerate(data_re[k]) if j != 0))
-        im_start.append(next(i for i,j in enumerate(data_im[k]) if j != 0))
+    start_indices.update({file_name:start_index})
+    print(start_indices)
+    print(file_name, " ", time.time()-t1)
 
-
-    print("re ", max(re_start))
-    print("im ", max(im_start))
-
+with open("start_indices", "w") as f:
+    yaml.safe_dump(start_indices, f)
