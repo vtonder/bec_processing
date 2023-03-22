0c567e8f6de7a5170bbaf6ba15006cb3e0615186
vereese van tonder <vereese@ska.ac.za>
2023-03-22 09:27:27 +0200

    Initial code optimisation for ps_sk

--- sk/ps_sk.py
+++ sk/ps_sk.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import sys
 sys.path.append('../')
-from constants import num_ch, start_indices, lower_limit, upper_limit, pulsars 
+from constants import num_ch, start_indices, lower_limit, upper_limit, pulsars, time_chunk_size
 sys.path.append('../pulsar_processing')
 import argparse
 
@@ -16,8 +16,9 @@ args = parser.parse_args()
 
 df = h5py.File('/net/com08/data6/vereese/' + args.file, 'r')
 start_index = start_indices[args.file]
+tot_ndp = df['Data/timestamps'].shape[0]
 num_data_points = df['Data/timestamps'].shape[0] - start_index 
-data = df['Data/bf_raw'][:,int(start_index):,:]
+#data = df['Data/bf_raw'][:,int(start_index):,:]
 tag = args.file[6:10] + '_'   # add last 4 digits of observation code onto the file_name
 pol = args.file[-5:-3]  # polarisation 0x or 0y
 
@@ -27,40 +28,69 @@ int_samples_T = int(np.floor(samples_T))
 
 num_pulses = int(np.floor(num_data_points / samples_T))  # number of vela pulses per observation
 M = int(args.M) 
+num_sk_chunk = time_chunk_size/M # number of SK's in 1 chunk
 
 sk = np.load("sk_M" + str(M) + "_" + tag + pol + ".npy")
 FFT_LEN = int(num_ch) 
 low = lower_limit[M] 
 up = upper_limit[M] 
 
-SK_flags = np.zeros([num_ch, int(num_data_points/M)]) # spans all the number of sk sets and indicated which ones were flagged
-SK_flags2 = np.zeros([num_ch, int(num_data_points)]) # make this so that it can be folded ito num vela pulses
-for i, idx in enumerate(np.arange(0, int(num_data_points), M)):
-    idx_start = int(idx)
-    idx_stop = int(idx_start + M)
-    if i == np.shape(sk)[1]:
-        break
+SK_flags = np.zeros([num_ch, int(num_data_points/M)], dtype=np.float16) # spans all the number of sk sets and indicated which ones were flagged
+SK_flags2 = np.zeros([num_ch, int(num_data_points)], dtype=np.float16) # make this so that it can be folded ito num vela pulses
+summed_profile = np.zeros([num_ch, int_samples_T], dtype=np.float16)
+summed_flags = np.zeros([num_ch, int_samples_T], dtype=np.float16)
 
-    if idx_stop > num_data_points+start_index:
-        idx_stop = num_data_points+start_index-1
-
-    for j, val in enumerate(sk[:, i]):
-        if val < low or val > up:
-            SK_flags[j, i] = 1
-            SK_flags2[j, idx_start:idx_stop] = np.ones(M) 
-            data[j, idx_start:idx_stop, 0] = data[600, 0:M, 0]
-            data[j, idx_start:idx_stop, 1] = data[600, 0:M, 1]
-
-summed_profile = np.zeros([num_ch, int_samples_T])
-summed_flags = np.zeros([num_ch, int_samples_T])
+print("***SHAPES***")
+print("sk             :", sk.shape)
+print("sk_flags       :", SK_flags.shape)
+print("sk_flags2      :", SK_flags2.shape)
+print("summed_profile :", summed_profile.shape)
+print("summed_flags   :", summed_flags.shape)
+print("************")
 
 for i in np.arange(num_pulses):
-    start = int(i * samples_T)
+    start = start_index + i*samples_T
     end = start + int_samples_T
-    if end >= num_data_points:
+    chunk_start = int(np.floor(start / time_chunk_size) * time_chunk_size)
+    chunk_stop = int(np.ceil(end / time_chunk_size) * time_chunk_size)
+
+    print("pulse i       : ", i)
+    #print("chunk_start   : ", chunk_start)
+    #print("chunk_stop    : ", chunk_stop)
+    #print("cstp - cstrt  : ", chunk_stop - chunk_start)
+
+    if chunk_stop >= tot_ndp:
         break
-    re = data[:, start:end, 0].astype(np.float)/128
-    im = data[:, start:end, 1].astype(np.float)/128
+    data = df['Data/bf_raw'][:, chunk_start:chunk_stop, :]
+    clean_data =  data[600, 0:M, 0]
+    # RFI mitigation
+    for j, idx in enumerate(np.arange(chunk_start, chunk_stop, M)):
+        idx_start = int(idx)
+        idx_stop = int(idx_start + M)
+        sk_idx = int(num_sk_chunk*i + j)
+        if idx_stop > tot_ndp:
+            print("shortening range because otherwise it will read from memory that doesn't exist")
+            print("tot_ndp : ", tot_ndp)
+            print("idx_stop: ", idx_stop)
+            idx_stop = tot_ndp - 1
+
+        for ch, val in enumerate(sk[:, sk_idx]):
+            if val < low or val > up:
+                SK_flags[ch, sk_idx] = 1
+                SK_flags2[ch, idx_start:idx_stop] = np.ones(M)
+                # TODO: improve this mitigation strategy. coincidentally ch 600 works for vela and J0536
+                data[ch, idx_start:idx_stop, 0] = clean_data
+                data[ch, idx_start:idx_stop, 1] = clean_data
+
+    pulse_start = int(start_index + (i*samples_T) - chunk_start)
+    pulse_stop = pulse_start + int_samples_T
+
+    #print("pulse_start      : ", pulse_start)
+    #print("pulse_stop       : ", pulse_stop)
+    #print("pulse stop-start : ", pulse_stop-pulse_start)
+
+    re = np.float16(data[:, pulse_start:pulse_stop, 0]/128)
+    im = np.float16(data[:, pulse_start:pulse_stop, 1]/128)
     summed_profile += re ** 2 + im ** 2
     summed_flags += SK_flags2[:, start:end]
 
