b4c552d9dd3228dd3d0c9d3b4b043f39302902b9
vereese van tonder <vereese@ska.ac.za>
2023-03-28 09:53:35 +0200

    Modified to run experiment on J0742

--- sk/sk.sh
+++ sk/sk.sh
@@ -1,20 +1,20 @@
  #!/bin/bash
 
 echo getting sk
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0x.h5
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0y.h5
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0x.h5 -m 1024
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0y.h5 -m 1024
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0x.h5 -m 2048
-mpiexec -n 30 python ps_sk_mpi.py 1604642762_wide_tied_array_channelised_voltage_0y.h5 -m 2048
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 512
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 512
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 1024
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 1024
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 2048
+mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 2048
 echo done
 echo calling ps
-python ps_sk_no_mit.py 1604642762_wide_tied_array_channelised_voltage_0x.h5
-python ps_sk_no_mit.py 1604642762_wide_tied_array_channelised_voltage_0y.h5
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0x.h5
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0y.h5
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0x.h5 -m 1024
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0y.h5 -m 1024
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0x.h5 -m 2048
-python ps_sk.py 1604642762_wide_tied_array_channelised_voltage_0y.h5 -m 2048
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 512
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 512
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 1024
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 1024
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 2048
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 2048
 echo done
