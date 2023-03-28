adb40afc50b6208b39ccaedd3f4e12ec863b3343
vereese van tonder <vereese@ska.ac.za>
2023-03-28 09:50:32 +0200

    Integrated ps_sk_no_mit.py into ps_sk

--- sk/ps_sk.py
+++ sk/ps_sk.py
@@ -7,11 +7,42 @@ from constants import num_ch, start_indices, lower_limit, upper_limit, pulsars, 
 sys.path.append('../pulsar_processing')
 import argparse
 
+def rfi_mitigation(data, sk_flags, sk_sum_flags, sk, pulse_i, num_sk_chunk, M, data_window_len, start_index, chunk_start):
+    print("***starting RFI mitigation***")
+    clean_data =  data[600, 0:M, 0]
+    for j, idx in enumerate(np.arange(0, data_window_len, M)):
+        idx_start = int(idx)
+        idx_stop = int(idx_start + M)
+        sk_idx = int(num_sk_chunk * pulse_i + j)
+        sk_sum_idx = int(chunk_start+idx_start-start_index)
+
+        #print("idx_start :", idx_start)
+        #print("idx_stop  :", idx_stop)
+        #print("sk_idx    :", sk_idx)
+
+        if idx_stop > tot_ndp:
+            print("shortening range because otherwise it will read from memory that doesn't exist")
+            print("tot_ndp : ", tot_ndp)
+            print("idx_stop: ", idx_stop)
+            idx_stop = tot_ndp - 1
+
+        for ch, val in enumerate(sk[:, sk_idx]):
+            if val < low or val > up:
+                sk_flags[ch, sk_idx] = 1
+                # TODO: verify that indices are correct
+                sk_sum_flags[ch, sk_sum_idx:sk_sum_idx+M] = np.ones(M, dtype=np.float16)
+                # TODO: improve this mitigation strategy. coincidentally ch 600 works for vela and J0536
+                data[ch, idx_start:idx_stop, 0] = clean_data
+                data[ch, idx_start:idx_stop, 1] = clean_data
+
+    return data, sk_flags, sk_sum_flags
+
+
 t1 = time.time()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="observation file to process. search path: /net/com08/data6/vereese/")
-parser.add_argument("-m", dest="M", help="Number of spectra to accumulate in SK calculation", default=512)
+parser.add_argument("-m", dest="M", help="Number of spectra to accumulate in SK calculation. Default is None. ie no RFI mitigation", default=0)
 args = parser.parse_args()
 
 df = h5py.File('/net/com08/data6/vereese/' + args.file, 'r')
@@ -27,23 +58,30 @@ samples_T = pulsar['samples_T']
 int_samples_T = int(np.floor(samples_T))
 
 num_pulses = int(np.floor(num_data_points / samples_T))  # number of vela pulses per observation
-M = int(args.M) 
-num_sk_chunk = time_chunk_size/M # number of SK's in 1 chunk
+M = int(args.M)
+num_sk_chunk = 0
+if M:
+    num_sk_chunk = time_chunk_size / M  # number of SK's in 1 chunk
 
 sk = np.load("sk_M" + str(M) + "_" + tag + pol + ".npy")
 FFT_LEN = int(num_ch) 
 low = lower_limit[M] 
 up = upper_limit[M] 
 
-SK_flags = np.zeros([num_ch, int(num_data_points/M)], dtype=np.float16) # spans all the number of sk sets and indicated which ones were flagged
-SK_flags2 = np.zeros([num_ch, int(num_data_points)], dtype=np.float16) # make this so that it can be folded ito num vela pulses
+sk_flags = np.zeros([num_ch, int(num_data_points / M)], dtype=np.float16) # spans all the number of sk sets and indicated which ones were flagged
+sk_sum_flags = np.zeros([num_ch, int(num_data_points)], dtype=np.float16) # make this so that it can be folded ito num vela pulses
 summed_profile = np.zeros([num_ch, int_samples_T], dtype=np.float16)
 summed_flags = np.zeros([num_ch, int_samples_T], dtype=np.float16)
 
+print("***DEBUG INFO***")
+print("start_index        : ", start_index)
+print("total data len     : ", tot_ndp)
+print("num_data_points    : ", num_data_points)
+print("num_pulses         : ", num_pulses)
 print("***SHAPES***")
 print("sk             :", sk.shape)
-print("sk_flags       :", SK_flags.shape)
-print("sk_flags2      :", SK_flags2.shape)
+print("sk_flags       :", sk_flags.shape)
+print("sk_flags2      :", sk_sum_flags.shape)
 print("summed_profile :", summed_profile.shape)
 print("summed_flags   :", summed_flags.shape)
 print("************")
@@ -63,51 +101,20 @@ for i in np.arange(num_pulses):
     if chunk_stop >= tot_ndp:
         break
     data = df['Data/bf_raw'][:, chunk_start:chunk_stop, :]
-    clean_data =  data[600, 0:M, 0]
-    print("shape data: ", data.shape)
-    print("shape clean data: ", clean_data.shape)
 
-    print("***starting RFI mitigation***")
-    # RFI mitigation
-    for j, idx in enumerate(np.arange(0, data_window_len, M)):
-        idx_start = int(idx)
-        idx_stop = int(idx_start + M)
-        sk_idx = int(num_sk_chunk*i + j)
-        data_idx = int(chunk_start+idx_start-start_index) 
-
-        #print("idx_start :", idx_start)
-        #print("idx_stop  :", idx_stop)
-        #print("sk_idx    :", sk_idx)
-
-        if idx_stop > tot_ndp:
-            print("shortening range because otherwise it will read from memory that doesn't exist")
-            print("tot_ndp : ", tot_ndp)
-            print("idx_stop: ", idx_stop)
-            idx_stop = tot_ndp - 1
-
-        for ch, val in enumerate(sk[:, sk_idx]):
-            if val < low or val > up:
-                SK_flags[ch, sk_idx] = 1
-                #TODO: verify that this is correct
-                SK_flags2[ch, data_idx:data_idx+M] = np.ones(M, dtype=np.float16)
-                # TODO: improve this mitigation strategy. coincidentally ch 600 works for vela and J0536
-                data[ch, idx_start:idx_stop, 0] = clean_data
-                data[ch, idx_start:idx_stop, 1] = clean_data
+    if M:
+        data, sk_flags, sk_sum_flags = rfi_mitigation(data, sk_flags, sk_sum_flags, sk, i, num_sk_chunk, M, data_window_len, start_index, chunk_start)
 
     pulse_start = int(start_index + (i*samples_T) - chunk_start)
     pulse_stop = pulse_start + int_samples_T
 
-    #print("pulse_start      : ", pulse_start)
-    #print("pulse_stop       : ", pulse_stop)
-    #print("pulse stop-start : ", pulse_stop-pulse_start)
-
     re = np.float16(data[:, pulse_start:pulse_stop, 0]/128)
     im = np.float16(data[:, pulse_start:pulse_stop, 1]/128)
     summed_profile += re ** 2 + im ** 2
-    summed_flags += SK_flags2[:, pulse_start:pulse_stop]
+    summed_flags += sk_sum_flags[:, pulse_start:pulse_stop]
 
 np.save('ps_M' + str(M) + "_" + tag + pol, summed_profile)
-np.save('SK_flags' + str(M) + "_" + tag + pol, SK_flags)
+np.save('SK_flags' + str(M) + "_" + tag + pol, sk_flags)
 np.save('summed_flags' + str(M) + "_" + tag + pol, summed_flags)
 print("procesing took: ", time.time() - t1)
 
