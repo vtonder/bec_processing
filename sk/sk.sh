63af82ae9f44284deacaf5105b4c875ee6a79919
vereese <vereese@ska.ac.za>
2023-04-04 10:28:06 +0200

    modified number of processes

--- sk/sk.sh
+++ sk/sk.sh
@@ -1,16 +1,16 @@
  #!/bin/bash
 
 echo getting sk
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 512
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 512
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 1024
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 1024
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 2048
-mpiexec -n 30 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 2048
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 512
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 512
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 1024
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 1024
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 2048
+mpiexec -n 32 python ps_sk_mpi.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 2048
 echo done
 echo calling ps
-python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5
-python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 #no mitigation
+python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 #no mitigation
 python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 512
 python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0y.h5 -m 512
 python ps_sk.py 1604643883_wide_tied_array_channelised_voltage_0x.h5 -m 1024
