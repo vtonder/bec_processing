34d6e9aa70b6f25305090d767da6d32a89a0ca6c
vereese van tonder <vereese@ska.ac.za>
2023-04-04 13:43:47 +0200

    Each processor only reads 1 chunk at a time

--- sk/bench_test_mpi.py
+++ sk/bench_test_mpi.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import sys
 sys.path.append('../')
-from constants import start_indices, time_chunk_size
+from constants import start_indices, time_chunk_size, pulsars
 from mpi4py import MPI
 import argparse
 
@@ -16,8 +16,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file", help="observation file to process. search path: /net/com08/data6/vereese/")
 parser.add_argument("-m", dest="M", help="Number of spectra to accumulate in SK calculation", default=512)
 args = parser.parse_args()
-M = int(args.M) 
 
+M = int(args.M)
 if rank == 0:
     t1 = time.time()
     print("start bench test at:", t1)
@@ -29,30 +29,40 @@ if rank == 0:
 df = h5py.File('/net/com08/data6/vereese/' + args.file, 'r')
 data = df['Data/bf_raw']
 start_index = start_indices[args.file]
+tag = args.file[6:10] # observation tag
 
-data_len = df['Data/timestamps'].shape[0] 
+pulsar = pulsars[tag]
+samples_T = pulsar['samples_T']
+int_samples_T = int(np.floor(samples_T))
+
+data_len = df['Data/timestamps'].shape[0]
 num_data_points = ((data_len - start_index) // (size*time_chunk_size)) * (size*time_chunk_size)
 num_data_points_rank = num_data_points / size
+num_chunks_rank = num_data_points_rank / time_chunk_size # number of chunks per processor 
 
 start = int(start_index + rank*num_data_points_rank)
 stop = int(start + num_data_points_rank)
 
 if rank == 0:
-    print("total data_len     :", data_len)
-    print("processing only    :", num_data_points)
-    print("data points rank   :", num_data_points_rank)
-    print("start_index        :", start_index)
-    print("start              :", start)
-    print("stop               :", stop)
-    print("M                  :", M)
+    print("total data_len       :", data_len)
+    print("processing only      :", num_data_points)
+    print("data points per rank :", num_data_points_rank)
+    print("start_index          :", start_index)
+    print("start                :", start)
+    print("stop                 :", stop)
+    print("M                    :", M)
 
     if num_data_points_rank % time_chunk_size:
         print("not respecting the chunk! number of data points to be processed per processor must be must be divisble by time_chunk_size: ", time_chunk_size, " remainder:", num_data_points_rank % time_chunk_size)
         exit()
 
-local_data = data[:, start:stop, :]  # get the portion of the array to be analyzed by each rank
+# Each processor only reads in 1 chunk at a time
+# TODO: also do a test bench test where reading in number chunks 1 pulse has. ie 63 chunks is 1 pulse
+for idx in np.arange(start, stop, time_chunk_size):
+    local_data = data[:, idx:idx+time_chunk_size, :]  # get the portion of the array to be analyzed by each rank
+
+df.close()
 
 if rank == 0:
     print("processing time    :", time.time() - t1)
-
 
