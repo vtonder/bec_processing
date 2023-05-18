83aa8fd5a8859c57bf400c4e146efd8748a47865
vereese van tonder <vereese@ska.ac.za>
2023-05-18 20:43:26 +0200

    added MPI

--- intensity.py
+++ intensity.py
@@ -1,3 +1,4 @@
+from mpi4py import MPI
 import h5py
 import numpy as np
 import time
@@ -6,7 +7,28 @@ sys.path.append('../')
 from constants import num_ch, start_indices, xy_time_offsets, pulsars, time_chunk_size
 import argparse
 
-t1 = time.time()
+
+def get_pulse_i(df, start_index, pulse_i, samples_T, int_samples_T, tot_ndp):
+    start = start_index + (pulse_i * samples_T)
+    end = start + int_samples_T
+    chunk_start = int(np.floor(start / time_chunk_size) * time_chunk_size)
+    chunk_stop = int(np.ceil(end / time_chunk_size) * time_chunk_size)
+    
+    if chunk_stop >= tot_ndp:
+        return -1
+
+    data = df['Data/bf_raw'][:, chunk_start:chunk_stop, :]
+    pulse_start = int(start_index + (pulse_i * samples_T) - chunk_start)
+    pulse_stop = pulse_start + int_samples_T
+    re = data[:, pulse_start:pulse_stop, 0].astype(np.float16)
+    im = data[:, pulse_start:pulse_stop, 1].astype(np.float16)
+
+    return np.float16((re**2 + im**2) / 128**2)
+
+# get number of processors and processor rank
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("tag", help="observation tag to process. search path: /net/com08/data6/vereese/")
@@ -36,64 +58,37 @@ if ndp_x <= ndp_y:
 else:
     ndp = ndp_y
 
-#data = df['Data/bf_raw'][:,int(start_index):,:]
-
 num_pulses = int(np.floor(ndp / samples_T))  # number of pulses per observation
-FFT_LEN = int(num_ch)
+np_rank = num_pulses / size # number of pulses per rank
 summed_profile = np.zeros([num_ch, int_samples_T], dtype=np.float16)
 
-print("*****INFO*****")
-print("processing            : ", pulsar['name'])
-print("start_index x pol     : ", si_x)
-print("start_index y pol     : ", si_y)
-print("total x pol data len  : ", tot_ndp_x)
-print("total y pol data len  : ", tot_ndp_y)
-print("num_data_points       : ", ndp)
-print("num_pulses            : ", num_pulses)
-print("summed_profile shape  : ", summed_profile.shape)
-print("**************")
+if rank == 0:
+    t1 = time.time()
+    print("*****INFO*****")
+    print("processing            : ", pulsar['name'])
+    print("start_index x pol     : ", si_x)
+    print("start_index y pol     : ", si_y)
+    print("total x pol data len  : ", tot_ndp_x)
+    print("total y pol data len  : ", tot_ndp_y)
+    print("num_data_points       : ", ndp)
+    print("num_pulses            : ", num_pulses)
+    print("summed_profile shape  : ", summed_profile.shape)
+    print("**************")
 
-for i in np.arange(num_pulses):
-    start_x = si_x + i*samples_T
-    end_x = start_x + int_samples_T
-    chunk_start_x = int(np.floor(start_x / time_chunk_size) * time_chunk_size)
-    chunk_stop_x = int(np.ceil(end_x / time_chunk_size) * time_chunk_size)
-    data_window_len_x = chunk_stop_x - chunk_start_x
-
-    start_y = si_y + i*samples_T
-    end_y = start_y + int_samples_T
-    chunk_start_y = int(np.floor(start_y / time_chunk_size) * time_chunk_size)
-    chunk_stop_y = int(np.ceil(end_y / time_chunk_size) * time_chunk_size)
-    data_window_len_y = chunk_stop_y - chunk_start_y
-
-    #print("pulse i         : ", i)
-    #print("chunk_start     : ", chunk_start)
-    #print("chunk_stop      : ", chunk_stop)
-    #print("data_window_len : ", data_window_len)
-
-    if chunk_stop_x >= tot_ndp_x:
+for i in np.arange(rank*np_rank, (rank+1)*np_rank):
+    data_x = get_pulse_i(dfx, si_x, i, samples_T, int_samples_T, tot_ndp_x)
+    data_y = get_pulse_i(dfy, si_y, i, samples_T, int_samples_T, tot_ndp_y)
+    if data_x == -1 or data_y == -1:
         break
+    summed_profile += data_x**2 + data_y**2
 
-    if chunk_stop_y >= tot_ndp_y:
-        break
-
-    data_x = dfx['Data/bf_raw'][:, chunk_start_x:chunk_stop_x, :]
-    data_y = dfy['Data/bf_raw'][:, chunk_start_y:chunk_stop_y, :]
-
-    pulse_start_x = int(si_x + (i*samples_T) - chunk_start_x)
-    pulse_stop_x = pulse_start_x + int_samples_T
-
-    pulse_start_y = int(si_y + (i*samples_T) - chunk_start_y)
-    pulse_stop_y = pulse_start_y + int_samples_T
-
-    re_x = data_x[:, pulse_start_x:pulse_stop_x, 0].astype(np.float16)
-    im_x = data_x[:, pulse_start_x:pulse_stop_x, 1].astype(np.float16)
-
-    re_y = data_y[:, pulse_start_y:pulse_stop_y, 0].astype(np.float16)
-    im_y = data_y[:, pulse_start_y:pulse_stop_y, 1].astype(np.float16)
-
-    summed_profile += (np.float16((re_x ** 2 + im_x ** 2)/128**2))**2 + (np.float16((re_y ** 2 + im_y ** 2)/128**2))**2
-
-np.save('intensity' + "_" + tag, summed_profile)
-print("processing took: ", time.time() - t1)
+if rank > 0:
+    comm.Send([summed_profile, MPI.DOUBLE], dest=0, tag=15)  # send results to process 0
+else:
+    for i in range(1, size):
+        tmp_summed_profile = np.zeros([num_ch, int_samples_T], dtype=np.float16)
+        comm.Recv([tmp_summed_profile, MPI.DOUBLE], source=i, tag=15)
+        summed_profile += tmp_summed_profile
+    np.save('intensity' + "_" + tag, summed_profile)
+    print("processing took: ", time.time() - t1)
 
