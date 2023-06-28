e1d089f0883817af67812926dcb3bf44c883eeb5
vereese van tonder <vereese@ska.ac.za>
2023-06-28 09:38:50 +0200

    Added option to calculate multi scale SK

--- sk/ps_sk_mpi.py
+++ sk/ps_sk_mpi.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 from constants import start_indices, time_chunk_size
 from mpi4py import MPI
 sys.path.append('../pulsar_processing')
-from kurtosis import spectral_kurtosis_cm
+from kurtosis import spectral_kurtosis_cm, ms_spectral_kurtosis_cm
 import argparse
 
 # get number of processors and processor rank
@@ -17,9 +17,17 @@ rank = comm.Get_rank()
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="observation file to process. search path: /net/com08/data6/vereese/")
 parser.add_argument("-m", dest="M", help="Number of spectra to accumulate in SK calculation", default=512)
+parser.add_argument("-s", dest="S", help="Multi-scale or single scale SK calculation. set to sk or msk", default="sk")
 args = parser.parse_args()
 
 M = int(args.M)
+
+if args.S == "sk":
+    file_prefix = "SK"
+    sk_func = spectral_kurtosis_cm
+else:
+    file_prefix = "MSK"
+    sk_func = ms_spectral_kurtosis_cm
 
 if rank == 0:
     t1 = time.time()
@@ -36,10 +44,9 @@ num_data_points = ((data_len - start_index) // (size*time_chunk_size)) * (size*t
 num_data_points_rank = num_data_points / size
 
 num_sk_rank = int(num_data_points_rank / M)  # number of sk per rank to process
-num_sk_chunk = int(time_chunk_size / M) # number sk in 1 chunk
+num_sk_chunk = int(time_chunk_size / M)  # number sk in 1 chunk
 start = int(start_index + rank*num_data_points_rank)
 stop = int(start + num_data_points_rank)
-#local_data = data[:, start:stop, :]  # get the portion of the array to be analyzed by each rank
 
 # SK RFI mitigation
 FFT_LEN = 1024
@@ -59,7 +66,8 @@ if rank == 0:
     print("SK shape           : ", np.shape(sk))
 
     if num_data_points_rank % time_chunk_size:
-        print("not respecting the chunk! number of data points to be processed per processor must be must be divisble by time_chunk_size: ", time_chunk_size, " remainder:", num_data_points_rank % time_chunk_size)
+        print("not respecting the chunk! number of data points to be processed per processor must be must be divisble "
+              "by time_chunk_size: ", time_chunk_size, " remainder:", num_data_points_rank % time_chunk_size)
         exit()
 
 # faster for each processor to just read and process 1 chunk at a time
@@ -67,9 +75,10 @@ for i, ld_idx in enumerate(np.arange(start, stop, time_chunk_size)):
     local_data = data[:, ld_idx:int(ld_idx+time_chunk_size), :]
     sk_idx_offset = i * num_sk_chunk
     for j, idx in enumerate(np.arange(0, time_chunk_size, M)):
-        sk[:, sk_idx_offset + j] = spectral_kurtosis_cm(local_data[:, idx:idx + M, 0] + 1j * local_data[:, idx:idx + M, 1], M, FFT_LEN * 2)
+        sk[:, sk_idx_offset + j] = sk_func(local_data[:, idx:idx + M, 0] + 1j*local_data[:, idx:idx + M, 1], M,
+                                           FFT_LEN * 2)
 
-# send results to rank 0
+# accumulate results
 if rank > 0:
     comm.Send([sk, MPI.DOUBLE], dest=0, tag=15)  # send results to process 0
 else:
@@ -78,11 +87,10 @@ else:
     for i in range(1, size):  # determine the size of the array to be received from each process
         tmp_SK = np.zeros([FFT_LEN, num_sk_rank], dtype=np.float16)
         comm.Recv([tmp_SK, MPI.DOUBLE], source=i, tag=15)  # receive SK results from the process
-        tot_SK[:,int(i*num_sk_rank):int((i+1)*num_sk_rank)] = tmp_SK
+        tot_SK[:, int(i*num_sk_rank):int((i+1)*num_sk_rank)] = tmp_SK
 
-    tag = '_' + args.file[6:10] + '_'   # add last 4 digits of observation code onto the file_name
-    pol = args.file[-5:-3] # polarisation 0x or 0y
+    tag = '_' + args.file[6:10] + '_'  # add last 4 digits of observation code onto the file_name
+    pol = args.file[-5:-3]  # polarisation 0x or 0y
 
-    np.save('sk_M' +str(M) + tag + pol , tot_SK)
+    np.save(file_prefix + '_M' + str(M) + tag + pol, tot_SK)
     print("procesing took: ", time.time() - t1)
-
