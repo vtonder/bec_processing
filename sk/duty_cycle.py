594405bbc704f14df4ffd1cb6d478f1ac9d433dd
vereese van tonder <vereese@ska.ac.za>
2023-07-13 11:28:06 +0200

    Investigate multi scale SK

--- sk/duty_cycle.py
+++ sk/duty_cycle.py
@@ -6,30 +6,57 @@ font = {'family': 'STIXGeneral',
 plt.rc('font', **font)
 
 M = 512
-duty_cycles = np.arange(100)
-
+duty_cycles = np.array([20,30,40,50,80,80,50,80,10,80,80,80,80,10,50,80,40,10,80,50,90,10,20,30,5,50,20,80])
+msklen = len(duty_cycles) - 4
+print("len dc: ", len(duty_cycles))
+print("len msklen: ", msklen )
 duty_samples = (duty_cycles / 100) * M
 wgn = np.random.normal(0, 1, size=M) + 1j*np.random.normal(0, 1, size=M)
 snrs = [1,2, 5,10,50]
 SK = [[], [], [], [], []]
-#S1 = []
-#S2 = []
+S1 = []
+S2 = []
 for i, snr in enumerate(snrs):
-    for dc in duty_samples:
+    S1.append([])
+    S2.append([])
+    for j, dc in enumerate(duty_samples):
         s = np.zeros([M])
         s[0:int(dc)] = snr
         s = wgn+s
         perio = np.abs(s)**2
-        S1 = perio.sum()  # like the mean of PSD u
-        S2 = np.sum(perio ** 2)
-        SK[i].append(((M + 1) / (M - 1)) * ((M * S2 / S1 ** 2) - 1))
+        S1[i].append(perio.sum())  # like the mean of PSD u
+        S2[i].append(np.sum(perio ** 2))
+        SK[i].append(((M + 1) / (M - 1)) * ((M * S2[i][j] / S1[i][j] ** 2) - 1))
 
-plt.figure(0, figsize=[22,16])
-plt.plot(duty_cycles, SK[0], label="SNR=1")
-plt.plot(duty_cycles, SK[1], label="SNR=2")
-plt.plot(duty_cycles, SK[2], label="SNR=5")
-plt.plot(duty_cycles, SK[3], label="SNR=10")
-plt.plot(duty_cycles, SK[4], label="SNR=50")
+MSK = [[], [], [], [], []]
+for i, snr in enumerate(snrs):
+    for j, s1 in enumerate(S1[i][:msklen]):
+        s1 = 0.25*(S1[i][j] + S1[i][j+1] + S1[i][j+2] + S1[i][j+3])
+        s2 = 0.25*(S2[i][j] + S2[i][j+1] + S2[i][j+2] + S2[i][j+3])
+        MSK[i].append(((M + 1) / (M - 1)) * ((M * s2 / s1 ** 2) - 1))
+
+"""plt.figure(0, figsize=[22,16])
+plt.plot(duty_cycles, SK[0], "o",label="SNR=1")
+plt.plot(duty_cycles, SK[1], "o",label="SNR=2")
+plt.plot(duty_cycles, SK[2], "o",label="SNR=5")
+plt.plot(duty_cycles, SK[3], "o",label="SNR=10")
+plt.plot(duty_cycles, SK[4], "o",label="SNR=50")
+plt.axhline(y=0.77511, xmin=0,xmax=100, linestyle='--')
+plt.axhline(y=1.3254, xmin=0, xmax=100, linestyle='--')
+plt.grid()
+plt.ylabel("Spectral Kurtosis")
+plt.ylim([-1,4])
+plt.xlim([0,95])
+plt.xlabel("Duty Cycle %")
+plt.legend()"""
+#plt.savefig('/home/vereese/Documents/PhD/jai-2e/duty.eps', bbox_inches='tight')
+
+plt.figure(1, figsize=[22,16])
+plt.plot(duty_cycles[:msklen], MSK[0], "o", label="SNR=1")
+plt.plot(duty_cycles[:msklen], MSK[1], "o", label="SNR=2")
+plt.plot(duty_cycles[:msklen], MSK[2], "o", label="SNR=5")
+plt.plot(duty_cycles[:msklen], MSK[3], "o", label="SNR=10")
+plt.plot(duty_cycles[:msklen], MSK[4], "o", label="SNR=50")
 plt.axhline(y=0.77511, xmin=0,xmax=100, linestyle='--')
 plt.axhline(y=1.3254, xmin=0, xmax=100, linestyle='--')
 plt.grid()
@@ -38,5 +65,7 @@ plt.ylim([-1,4])
 plt.xlim([0,95])
 plt.xlabel("Duty Cycle %")
 plt.legend()
-plt.savefig('/home/vereese/Documents/PhD/jai-2e/duty.eps', bbox_inches='tight')
+#plt.savefig('/home/vereese/Documents/PhD/jai-2e/duty.eps', bbox_inches='tight')
+
+
 plt.show()
