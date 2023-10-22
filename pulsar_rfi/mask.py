f167b19ac5d860f2e6386f57965275611b56adbe
vereese van tonder <vereese@ska.ac.za>
2023-10-22 17:50:37 +0200

    Make mask the same indices as apply_mask

--- pulsar_rfi/mask.py
+++ pulsar_rfi/mask.py
@@ -5,21 +5,21 @@ import numpy as np
 # GNSS: 1145.85 - 1300.9 MHz <=> 347 - 532
 # GNSS: 1519.35 - 1608.3 MHz <=> 794 - 900
 
-I = np.load("/home/vereese/git/phd_data/intensity_2210.npy")
+I = np.load("/home/vereese/git/phd_data/sk_analysis/2210/intensity_2210.npy")
 
-masked_I = np.zeros(np.shape(I))
-masked_sf = 100*np.ones(1024)
-masked_I[51:95,:] = I[51:95,:]
-masked_sf[51:95] = np.zeros(44)
-
-masked_I[126:347,:] = I[126:347,:]
-masked_sf[126:347] = np.zeros(221)
-
-masked_I[533:794,:] = I[533:794,:]
-masked_sf[533:794] = np.zeros(261)
-
-masked_I[901:974,:] = I[901:974,:]
-masked_sf[901:974] = np.zeros(73)
+masked_I = I
+masked_sf = 100*np.zeros(1024)
+ns = I.shape[1]
+masked_I[0:50,:] = np.zeros([50, ns])
+masked_sf[0:50] = 1
+masked_I[95:125,:] = np.zeros([30, ns])
+masked_sf[95:125] = 1
+masked_I[347:532,:] = np.zeros([185, ns])
+masked_sf[347:532] = 1
+masked_I[794:900,:] = np.zeros([106, ns])
+masked_sf[794:900] = 1
+masked_I[-50:,:] = np.zeros([50, ns])
+masked_sf[-50:] = 1
 
 np.save("masked_intensity_2210", masked_I)
 np.save("masked_sf_2210", masked_sf)
