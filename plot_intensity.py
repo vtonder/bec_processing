0c0c721349665f9f9f558bff71083ac51b373ab7
vereese <vereese@ska.ac.za>
2023-11-06 15:04:48 +0200

    Plot SK_flags for vela

--- plot_intensity.py
+++ plot_intensity.py
@@ -18,18 +18,54 @@ parser = argparse.ArgumentParser()
 parser.add_argument("tag", help="observation tag to plot intensity")
 args = parser.parse_args()
 
-data = np.load("/home/vereese/git/phd_data/intensity_" + args.tag + ".npy")
-#for i in np.arange(1024):
-#    data[i,:] = data[i,:] - np.mean(data[i,:])
+#data = np.load("/home/vereese/git/phd_data/intensity_" + args.tag + ".npy")
+DIR = "/home/vereese/data/phd_data/sk_analysis/1234/lower/"
+d1 = np.load(DIR + "SK_flags512_1234_0x.npy")
+d2 = np.load(DIR + "SK_flags1024_1234_0x.npy")
+d3 = np.load(DIR + "SK_flags2048_1234_0x.npy")
+d4 = np.load(DIR + "SK_flags10240_1234_0x.npy")
+data = np.load("intensity_xpol_1234.npy")
+
+for i in np.arange(1024):
+    data[i,:] = data[i,:] - np.mean(data[i,:])
 #profile = data.sum(axis=0)
 #mp = list(profile).index(max(list(profile)))
 #print("max point index: ", mp)
 #num_2_roll = int(len(profile)/2 - mp)
+#maxi = np.max(data[300:600,:])
+#mini = np.min(data[300:600,:])
 
+profile = data.sum(axis=0)
+mp = list(profile).index(max(list(profile)))
+print("max point index: ", mp)
+num_2_roll = int(len(profile)/2 - mp)
 
-plt.figure()
-plt.imshow(data, origin="lower", aspect="auto", extent=[0, 1, 856, 1712])
+plt.figure(0)
+plt.imshow(np.roll(data,num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 856, 1712])#, vmin=mini, vmax=maxi)
 plt.xlabel("pulse phase")
 plt.ylabel("frequency [MHz]")
-plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/p4511.eps', bbox_inches='tight')
-plt.show()
+plt.savefig('/home/vereese/far', bbox_inches='tight')
+
+'''plt.figure(0)
+plt.imshow(d1, origin="lower", aspect="auto", extent=[0, 10, 856, 1712])
+plt.xlabel("observation time [s]")
+plt.ylabel("frequency [MHz]")
+plt.savefig('/home/vereese/sk_flags_512', bbox_inches='tight')
+
+plt.figure(1)
+plt.imshow(d2, origin="lower", aspect="auto", extent=[0, 10, 856, 1712])
+plt.xlabel("observation time [s]")
+plt.ylabel("frequency [MHz]")
+plt.savefig('/home/vereese/sk_flags_1024', bbox_inches='tight')
+
+plt.figure(3)
+plt.imshow(d2, origin="lower", aspect="auto", extent=[0, 10, 856, 1712])
+plt.xlabel("observation time [s]")
+plt.ylabel("frequency [MHz]")
+plt.savefig('/home/vereese/sk_flags_2048', bbox_inches='tight')
+
+plt.figure(2)
+plt.imshow(d3, origin="lower", aspect="auto", extent=[0, 10, 856, 1712])
+plt.xlabel("observation time [s]")
+plt.ylabel("frequency [MHz]")
+plt.savefig('/home/vereese/sk_flags_10240', bbox_inches='tight')'''
