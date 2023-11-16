b91cccdce91d6f77394047697b72641bebfd763d
vereese <vereese@ska.ac.za>
2023-11-16 15:14:44 +0200

    Replace flagged data with zero instead of gaussian noise
    - keep track of number of non-zero accumulations in both intensity due
    to dropped packets and in sk_intensity due to flagged RFI
    - Added sk support to msk
    - refactoring in common and common_sk

--- sk/msk.py
+++ sk/msk.py
@@ -12,6 +12,7 @@ parser.add_argument("file", help="observation file to process. search path: /net
 parser.add_argument("-M", dest="M", help="Number of spectra to accumulate in SK calculation", default=512)
 parser.add_argument("-m", dest="m", help="Number of time samples to add up in MSK", default=1)
 parser.add_argument("-n", dest="n", help="Number of ch to add up in MSK", default=1)
+parser.add_argument("-f", dest="file_prefix", help="prefix to the output files", default="msk")
 
 args = parser.parse_args()
 
@@ -25,8 +26,7 @@ S1 = np.float32(np.load("S1_M" + str(M) + tag + pol + ".npy"))
 S2 = np.float32(np.load("S2_M" + str(M) + tag + pol + ".npy"))
 
 # SK RFI mitigation
-FFT_LEN = 1024
 sk = np.float32(ms_spectral_kurtosis_cm(S1, S2, M, m=m, n=n))
 
-np.save('MSK_M' + str(M) + "_m" + str(m) + "_n" + str(n) + tag + pol, sk)
+np.save(args.file_prefix + '_M' + str(M) + "_m" + str(m) + "_n" + str(n) + tag + pol, sk)
 print("procesing took: ", time.time() - t1)
