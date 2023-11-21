58f2a0ffaeaf0429ba2869722fbabb5cb80778e0
vereese <vereese@ska.ac.za>
2023-11-21 08:59:19 +0200

    Double square bug fix

--- intensity_snr.py
+++ intensity_snr.py
@@ -7,30 +7,7 @@ sys.path.append("../")
 from constants import num_ch, start_indices, pulsars, xy_time_offsets, time_chunk_size, upper_limit, lower_limit
 from pulsar_processing.pulsar_functions import incoherent_dedisperse
 import argparse
-from kurtosis import spectral_kurtosis_cm
-
-def rfi_mitigation(data, M, data_window_len):
-    std_re = np.sqrt(np.var(data[600,:,0]))
-    std_im = np.sqrt(np.var(data[600,:,1]))
-
-    for idx in np.arange(0, data_window_len, M):
-        idx_start = int(idx)
-        idx_stop = int(idx_start + M)
-
-        sk = spectral_kurtosis_cm(data[:, idx_start:idx_stop, 0] + 1j*data[:, idx_start:idx_stop, 1], M, 2048)
-
-        if idx_stop >= ndp:
-            print("shortening range because otherwise it will read from memory that doesn't exist")
-            print("tot_ndp : ", ndp)
-            print("idx_stop: ", idx_stop)
-            idx_stop = ndp - 1
-
-        for ch, val in enumerate(sk):
-            if val < low or val > up:
-                data[ch, idx_start:idx_stop, 0] = np.random.normal(0,std_re,1)[0]
-                data[ch, idx_start:idx_stop, 1] = np.random.normal(0,std_im,1)[0]
-
-    return data
+from matplotlib import pyplot as plt
 
 def get_data_window(start_index, pulse_i, samples_T, int_samples_T, tot_ndp):
     start = start_index + (pulse_i * samples_T)
@@ -59,7 +36,6 @@ rank = comm.Get_rank()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("tag", help="observation tag to process. search path: /net/com08/data6/vereese/")
-parser.add_argument("-m", dest="M", help="Number of spectra to accumulate in SK calculation", default=512)
 args = parser.parse_args()
 
 fx = '160464' + args.tag + '_wide_tied_array_channelised_voltage_0x.h5'
@@ -73,10 +49,6 @@ si_y = start_indices[fy] + xy_time_offsets[fy]
 
 tot_ndp_x = dfx['Data/timestamps'].shape[0] # total number of data points of x polarisation
 tot_ndp_y = dfy['Data/timestamps'].shape[0]
-
-M = int(args.M)
-low = lower_limit[M]
-up = upper_limit[M]
 
 tag = args.tag
 pulsar = pulsars[tag]
@@ -112,11 +84,11 @@ if rank == 0:
 
 prev_start_x, prev_stop_x = 0, 0
 prev_start_y, prev_stop_y = 0, 0
-pulse_start = 3042
-pulse_stop = 3153
+# to get pulse start and stop read off FWHM from intensity profile: ((4.9270-3.97325)/2) + 3.97325 = 4.450125 [units in 10**10] 
+pulse_start = 3035 
+pulse_stop = 3162
 pulse_w = pulse_stop - pulse_start
 snrs = np.zeros(np_rank)
-
 for i, pulse_i in enumerate(np.arange(rank * np_rank, (rank + 1) * np_rank)):
     chunk_start_x, chunk_stop_x = get_data_window(si_x, pulse_i, samples_T, int_samples_T, tot_ndp_x)
     chunk_start_y, chunk_stop_y = get_data_window(si_y, pulse_i, samples_T, int_samples_T, tot_ndp_y)
@@ -137,29 +109,26 @@ for i, pulse_i in enumerate(np.arange(rank * np_rank, (rank + 1) * np_rank)):
         prev_start_y = chunk_start_y
         prev_stop_y = chunk_stop_y
 
-    data_x = rfi_mitigation(data_x, M, data_len_x)
-    data_y = rfi_mitigation(data_y, M, data_len_y)
-
     sp_x = get_pulse_power(data_x, chunk_start_x, si_x, pulse_i, samples_T, int_samples_T)
     sp_y = get_pulse_power(data_y, chunk_start_y, si_y, pulse_i, samples_T, int_samples_T)
-    profile = np.float32(incoherent_dedisperse( np.float32(sp_x**2) + np.float32(sp_y**2), tag))
+    profile = np.float32(incoherent_dedisperse(sp_x + sp_y, tag)).sum(axis=0)
     snr = 0
     m = np.mean(profile[0:pulse_w])
     s = np.std(profile[0:pulse_w])
-    for i in np.arange(pulse_w):
-        snr = snr + (profile[pulse_start + i] - m)
+    for j in np.arange(pulse_w):
+        snr = snr + (profile[pulse_start + j] - m)
     snr = snr / (s * np.sqrt(pulse_w))
     snrs[i] = snr
 
 if rank > 0:
     comm.Send([snrs, MPI.DOUBLE], dest=0, tag=15)  # send results to process 0
 else:
-    tot_snrs = np.zeros(np_rank*rank)
+    tot_snrs = np.zeros(np_rank*size)
     tot_snrs[0:np_rank] = snrs
     for i in range(1, size):
-        tmp_snrs = np.zeros(np_rank, dtype=np.float32)
+        tmp_snrs = np.zeros(np_rank)
         comm.Recv([tmp_snrs, MPI.DOUBLE], source=i, tag=15)
-        tot_snrs[i*np_rank:i*np_rank + np_rank] = np.float32(tmp_snrs)
+        tot_snrs[i*np_rank:i*np_rank + np_rank] = tmp_snrs
 
-    np.save('snrs', summed_profile)
+    np.save('snrs', tot_snrs)
     print("processing took: ", time.time() - t1)
