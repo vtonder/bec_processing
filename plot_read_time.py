11c268e97a341e0d545bcaf18a2a4333b1d3cb71
vereese van tonder <vereese@ska.ac.za>
2023-12-12 14:05:23 +0200

    refactored + cosmetics

--- plot_read_time.py
+++ plot_read_time.py
@@ -1,36 +1,42 @@
 from matplotlib import pyplot as plt
+from constants import a4_textheight, a4_textwidth, thesis_font
 
-textwidth = 9.6 # 128.0 / 25.4 #
-textheight = 7 # 96.0 / 25.4 # 7
-plt.rc('font', size=12, family='STIXGeneral')
+# This data was generated using the time_reading_mpi.py script
+
+textwidth = a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+
+plt.rc('font', size=font_size, family='STIXGeneral')
 plt.rc('pdf', fonttype=42)
 #plt.rc('axes', titlesize=14, labelsize=14)
-plt.rc('axes', titlesize=12, labelsize=12)
-plt.rc(('xtick', 'ytick'), labelsize=12)
-plt.rc('legend', fontsize=12)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
 plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
 plt.rc('mathtext', fontset='stix')
 
 processing_time ={
-  '5': 3034.3588042259216,
-  '10':1574.2632755279542,
-  '15':1047.1665592193604,
-  '20':789.898957490921,
-  '25':709.3646445274353,
-  '30':672.2443092823029,
-  '35':643.1528521537781,
-  '40':576.8024142742157,
-  '45':537.296351480484
+  '5':95.52786914348601,
+  '10':45.981049015522004,
+  '15':31.585434335867564,
+  '20':24.818814676404,
+  '25':22.571849013328556,
+  '30':23.0546128765742,
+  '35':19.676805268696377,
+  '40':20.241357430815693,
+  '45':21.547988188531665,
+  '50':21.39174386906624
 }
 
 num_proc = [int(i) for i in processing_time.keys()]
 
 plt.figure(0)
-plt.plot(num_proc, list(processing_time.values()))
+plt.plot(num_proc, list(processing_time.values()), linewidth=2)
 plt.xlim(num_proc[0], num_proc[-1])
 plt.grid()
 plt.xlabel("Number of processors")
-plt.ylabel("Total processing time (s)")
+plt.ylabel("Average reading time (s)")
 plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/proctime', bbox_inches='tight')
 plt.show()
