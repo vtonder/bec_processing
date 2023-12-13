cecc67543363a3bcd8ef43277909c8d6cf30ca7a
vereese van tonder <vereese@ska.ac.za>
2023-12-13 14:17:29 +0200

    Made 2 functions in median
    one for applying median filter to 1 intensity file and another to apply
    median filter to intensities that has been sk rfi mitigated with
    different M values

--- pulsar_rfi/median_rfi.py
+++ pulsar_rfi/median_rfi.py
@@ -6,11 +6,6 @@ from constants import num_ch, pulsars
 import time
 import scipy
 
-t1 = time.time()
-I = np.load("/home/vereese/git/phd_data/intensity_2210.npy")
-int_samples_T = I.shape[1]
-pulsar = pulsars['2210']
-
 def median_smoothing(I, window_len=21):
     I_median = np.zeros(I.shape)
     wl_2 = int(window_len/2)
@@ -23,36 +18,71 @@ def median_smoothing(I, window_len=21):
 
     return diff
 
-def rfi_mit(I, diff, sf):
-    global int_samples_T
+def rfi_mit(I, diff, sf, int_samples_T, num_nz):
     for phi in np.arange(int_samples_T):
         data = I[:, phi]
         filtered_data = [d for d in data if d != 0]
         std = np.std(filtered_data)
+
         sf[:, phi] = [1 if diff[ch, phi] >= 4 * std else 0 for ch in np.arange(num_ch)]
-        I[:, phi] = [0 if diff[ch, phi] >= 4 * std else I[ch, phi] for ch in np.arange(num_ch)]
+        I[:, phi] = np.where(diff[:, phi] >= 4 * std, 0, I[:, phi])
+        # zero number of non-zero data points that went into I accumulation
+        num_nz[:, phi] = np.where(diff[:, phi] >= 4 * std, 0, num_nz[:, phi])
 
-    return I, sf
+    return I, sf, num_nz
 
-sf = np.zeros(I.shape)
-#diff = median_smoothing(I, 21)
-I_median = scipy.ndimage.median_filter(I,[21,1], mode="wrap")
-diff = I - I_median
+def sk_med():
+    M = ["64", "128", "256", "512", "1024", "2048", "4096", "8192"]
+    for m in M:
+        print(m)
+        I = np.load("/home/vereese/git/phd_data/sk_analysis/2210/l4sig/sk_intensity_l4sig_M" + m + "_m1_n1_2210_p45216.npy")
+        num_nz = np.load("/home/vereese/git/phd_data/sk_analysis/2210/l4sig/sk_num_nz_l4sig_M" + m + "_m1_n1_2210_p45216.npy")
+        int_samples_T = I.shape[1]
+        sf = np.zeros(I.shape)
+        I_median = scipy.ndimage.median_filter(I,[21,1], mode="wrap")
+        diff = np.abs(I - I_median)
 
-for i in np.arange(4):
-    sf_tmp = np.zeros(I.shape)
-    I, sf_tmp = rfi_mit(I, diff, sf_tmp)
-    sf = sf + sf_tmp
+        for i in np.arange(4):
+            sf_tmp = np.zeros(I.shape)
+            I, sf_tmp, num_nz = rfi_mit(I, diff, sf_tmp, int_samples_T, num_nz)
+            sf = sf + sf_tmp
 
-np.save("median_smoothed_I4_custom.npy", I)
-np.save("sf_median_custom", sf)
+        np.save("median_z_r4_sk_l4sig_M" + m + "_2210_p4812.npy", I)
+        np.save("nz_median_z_r4_sk_l4sig_M" + m + "_2210_p4812.npy", num_nz)
+        np.save("sf_median_z_r4_sk_l4sig_M" + m + "_2210_p4812.npy", sf)
+
+def int_med():
+    I = np.load("/home/vereese/git/phd_data/sk_analysis/2210/intensity_z_2210_p45216.npy")
+    num_nz = np.load("/home/vereese/git/phd_data/sk_analysis/2210/num_nz_z_2210_p45216.npy")
+    int_samples_T = I.shape[1]
+    sf = np.zeros(I.shape)
+    I_median = scipy.ndimage.median_filter(I, [21, 1], mode="wrap")
+    diff = np.abs(I - I_median)
+
+    for i in np.arange(4):
+        sf_tmp = np.zeros(I.shape)
+        I, sf_tmp, num_nz = rfi_mit(I, diff, sf_tmp, int_samples_T, num_nz)
+        sf = sf + sf_tmp
+
+    # g: dropped packets were replaced with Gaussian noise when calculating intensity
+    # z: dropped packets were left as 0s calculating intensity
+    # ran RFI mitigation 4 times => r4
+    np.save("median_z_r4_2210_p4812", I)
+    np.save("sf_median_z_2210_p4812", sf)
+    np.save("num_nz_median_z_2210_p4812", num_nz)
+
+t1 = time.time()
+int_med()
 print("processing time took: ", time.time()-t1)
 
-plt.figure(0)
-plt.imshow(I, origin="lower", aspect="auto")
+#plt.figure(0)
+#plt.imshow(I, origin="lower", aspect="auto")
 
-plt.figure(1)
-plt.plot((sf.sum(axis=1)*100)/(int_samples_T*4))
+#plt.figure(1)
+#plt.plot((sf.sum(axis=1)*100)/(int_samples_T*4))
 
-plt.show()
+#plt.figure(2)
+#plt.plot(power_mit.sum(axis=1))
+
+#plt.show()
 
