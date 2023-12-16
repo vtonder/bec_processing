e989e56cbf620242d4fd940ea77c08130907fdc1
vereese van tonder <vereese@ska.ac.za>
2023-12-16 10:22:17 +0200

    Place dropped packets at random instead of consecutively.
    Note it still shows the same relation.

--- sk/dropped_packets.py
+++ sk/dropped_packets.py
@@ -1,12 +1,19 @@
+import random
 import numpy as np
-from kurtosis import spectral_kurtosis, spectral_kurtosis_cm
+from kurtosis import spectral_kurtosis_cm
 from matplotlib import pyplot as plt
-from constants import upper_limit, upper_limit7, lower_limit, lower_limit7
+from constants import a4_textheight, a4_textwidth, thesis_font
+
+"""
+This script investigates the effect of zeros in the data on the SK.
+It was found that adding zeros to the data set raises the SK.
+Therefore, if you have dropped packets in your data then you'll SK will be more that increase your SK
+"""
 
 # Setup fonts and sizes for publication, based on page dimensions in inches
-textwidth =  128.0 / 25.4 #
-textheight = 96.0 / 25.4
-font_size = 11
+textwidth =  a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
 plt.rc('font', size=font_size, family='STIXGeneral')
 plt.rc('pdf', fonttype=42)
 #plt.rc('axes', titlesize=14, labelsize=14)
@@ -18,27 +25,27 @@ plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
 plt.rc('mathtext', fontset='stix')
 
 M = 512
-num_sk = 100000
-
-perc = np.arange(0,0.2,0.01)
-# NOTE: Adding 0s to the data raises the SK. Therefore, if you have dropped packets then you'll increase your SK
-#
+num_sk = 10000
+num_dp = M * num_sk # number of data points
+perc = np.arange(0, 0.2, 0.01)
 #print("N: ", N, " x.shape: ", x.shape)
-print("% 0s:", 100 * np.sum(np.where(perc == 0, True, False)) / (M*num_sk))
+print("% 0s:", 100 * np.sum(np.where(perc == 0, True, False)) / (M * num_sk))
 
 #XF = np.fft.fft(x, axis=1)
 mean_sk = []
 for p in perc:
-    x = np.random.randn(num_sk, M) + 1j * np.random.randn(num_sk, M)
-    x[:, 0:int(M*p)] = 0
+    # randn generates noise from the standard Gaussian distribution ie mean = 0; std = 1 = var
+    x = np.random.randn(num_dp) + 1j * np.random.randn(num_dp)
+    ndpz = int(num_sk * M * p) # number of data points to zero
+    indices = random.sample(range(num_dp - 1), ndpz)
+    x[indices] = 0
+    x = x.reshape(num_sk, M)
     mean_sk.append(np.mean(spectral_kurtosis_cm(x, M, 1024)))
 
 plt.figure()
 plt.plot(perc*100, (np.asarray(mean_sk) - 1)*100, linewidth=2)
 plt.xlabel("% dropped packets")
-plt.ylabel("% increase in SK")
+plt.ylabel("% increase in $\overline{SK}$")
 plt.xlim([perc[0]*100, perc[-1]*100])
-plt.savefig('/home/vereese/Documents/PhD/CASPER2023/casper_presentation/drop.eps', bbox_inches='tight')
-#plt.axhline(lower_limit7[M], linestyle = '--', linewidth=2, label="4 $\sigma$ thresholds")
-#plt.axhline(upper_limit7[M], linestyle = '--', linewidth=2)
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/drop.eps', bbox_inches='tight')
 plt.show()
