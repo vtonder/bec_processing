55fe9acd4188c82c8f9febb2c47402c42d4b4040
vereese van tonder <vereese@ska.ac.za>
2023-12-20 10:59:10 +0200

    Added MSK update TODO

--- sk/duty_cycle.py
+++ sk/duty_cycle.py
@@ -1,23 +1,22 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from constants import thesis_font, a4_textwidth, a4_textheight
 
-#font = {'family': 'STIXGeneral',
-#        'size': 42}
-#plt.rc('font', **font)
-
-textwidth = 9.6 #128.0 / 25.4
-textheight = 7 #96.0 / 25.4
-plt.rc('font', size=12, family='STIXGeneral')
-plt.rc('pdf', fonttype=42)
+textwidth = a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+plt.rc('font', size = font_size, family='STIXGeneral')
+plt.rc('pdf', fonttype = 42)
 #plt.rc('axes', titlesize=14, labelsize=14)
-plt.rc('axes', titlesize=12, labelsize=12)
-plt.rc(('xtick', 'ytick'), labelsize=12)
-plt.rc('legend', fontsize=12)
-plt.rc('lines', markersize=5)
+plt.rc('axes', titlesize = font_size, labelsize = font_size)
+plt.rc(('xtick', 'ytick'), labelsize = font_size)
+plt.rc('legend', fontsize = font_size)
+plt.rc('lines', markersize = 5)
 plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
 plt.rc('mathtext', fontset='stix')
 
 M = 512
+# duty cycle ito %
 duty_cycles = np.array([45,20,95,30,95,5,95,15,75,9,70,20,80,55,60,97,9,70,19,85,65,88,39,75,55,10])
 #duty_cycles = np.arange(0,110)
 msklen = len(duty_cycles) - 2
@@ -40,7 +39,11 @@ for i, snr in enumerate(snrs):
         S1[i].append(perio.sum())  # like the mean of PSD u
         S2[i].append(np.sum(perio ** 2))
         SK[i].append(((M + 1) / (M - 1)) * ((M * S2[i][j] / S1[i][j] ** 2) - 1))
+        if snr == 2 and j == 0:
+            plt.figure(3)
+            plt.plot(S1[i])
 
+# TODO: this is still the old wrong MSK implementation and needs to be updated using effective M and voting strategy
 MSK = [[], [], [], [], []]
 for i, snr in enumerate(snrs):
     for j, s1 in enumerate(S1[i][:msklen]):
@@ -48,7 +51,7 @@ for i, snr in enumerate(snrs):
         x2 = 0.5*(float(S2[i][j]) + float(S2[i][j+1])) # + S2[i][j+2] + S2[i][j+3])
         MSK[i].append(((M + 1) / (M - 1)) * ((M * x2 / x1 ** 2) - 1))
 
-plt.figure(0)#, figsize=[22,16])
+plt.figure(0)
 plt.plot(duty_cycles, SK[0], 'o',label="SNR=1")
 plt.plot(duty_cycles, SK[1], 'o',label="SNR=2")
 plt.plot(duty_cycles, SK[2], 'o',label="SNR=5")
@@ -65,7 +68,8 @@ plt.xlabel("Duty Cycle %")
 plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/duty2.eps', bbox_inches='tight')
 print("msk len: ", MSK)
 print("dc len: ", len(duty_cycles))
-plt.figure(1)#, figsize=[22,16])
+
+plt.figure(1)
 #plt.plot(duty_cycles[:msklen], MSK[0], "o", label="SNR=1", linewidth=2)
 #plt.plot(duty_cycles[:msklen], MSK[1], "o", label="SNR=2", linewidth=2)
 plt.plot(duty_cycles, SK[2], "o", label="SK", linewidth=2)
