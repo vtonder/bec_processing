3a3c8ba245c4e7327ee947ecf6cc4e956f95a61e
vereese van tonder <vereese@ska.ac.za>
2024-01-12 08:54:01 +0200

    Changed to latex font and pdf

--- spectral_inversion.py
+++ spectral_inversion.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from constants import a4_textwidth, a4_textheight, thesis_font
 
-textwidth = 9.6 # 128.0 / 25.4 #
-textheight = 7 # 96.0 / 25.4 # 7
-plt.rc('font', size=12, family='STIXGeneral')
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth = a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
 #plt.rc('axes', titlesize=14, labelsize=14)
-plt.rc('axes', titlesize=12, labelsize=12)
-plt.rc(('xtick', 'ytick'), labelsize=12)
-plt.rc('legend', fontsize=12)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
-plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-plt.rc('mathtext', fontset='stix')
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
 
 # frequency units Hz
 f1 = np.arange(1,6)*10.0
@@ -42,5 +51,5 @@ plt.xlabel('frequency [Hz]')
 plt.ylabel('X(n)')
 plt.legend()
 plt.xlim([-50, 49])
-plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/spec_inver.eps', bbox_inches='tight')
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/spec_inver.pdf', bbox_inches='tight')
 plt.show()
