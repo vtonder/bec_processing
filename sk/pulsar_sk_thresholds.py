b326f8ca7b2f3fd54a10bb4d4491517d72f598d8
vereese van tonder <vereese@ska.ac.za>
2024-01-14 14:09:55 +0200

    Making histogram plots for SK max and min section of thesis

--- sk/pulsar_sk_thresholds.py
+++ sk/pulsar_sk_thresholds.py
@@ -1,8 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
+sys.path.append("../")
+from constants import a4_textwidth, a4_textheight, thesis_font, frequencies
 
-#M = ["64"] #, "128", "256", "512", "1024", "2048", "4096", "8192"]
-M = ["128", "256", "512", "1024", "2048", "4096", "8192"]
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth = a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
+plt.rc('pdf', fonttype=42)
+#plt.rc('axes', titlesize=14, labelsize=14)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
+plt.rc('lines', markersize=5)
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
+
+M = ["64", "128", "256", "512", "1024", "2048", "4096", "8192"]
 
 #DIR = "/home/vereese/data/phd_data/sk_analysis/2210.bac/"
 #DIR = "/home/vereese/git/bec_processing/sk/2210.bac/"
@@ -16,7 +38,37 @@ ch4 = np.arange(532,794)
 ch5 = np.arange(924,974)
 clean_chs = np.concatenate([ch1,ch2,ch3,ch4,ch5])
 
-for m in M:
+"""
+# A plot to show clean channels
+fig, ax = plt.subplots()
+band_x = np.load("/home/vereese/git/phd_data/mean_analysis/2210/var_0x_1024.npy")
+band_y = np.load("/home/vereese/git/phd_data/mean_analysis/2210/var_0y_1024.npy")
+band = np.sqrt(band_x)
+ax.plot(frequencies, band)
+ax.set_xlim([frequencies[0], frequencies[-1]])
+ax.set_ylim([0, 28])
+ax.set_xlabel("frequency [MHz]")
+ax.set_ylabel("$\sigma_X$")
+plt.axvspan(frequencies[int(ch1[0])], frequencies[int(ch1[-1])], color='blue', alpha=0.5)
+plt.axvspan(frequencies[int(ch2[0])], frequencies[int(ch2[-1])], color='blue', alpha=0.5)
+plt.axvspan(frequencies[int(ch3[0])], frequencies[int(ch3[-1])], color='blue', alpha=0.5)
+plt.axvspan(frequencies[int(ch4[0])], frequencies[int(ch4[-1])], color='blue', alpha=0.5)
+plt.axvspan(frequencies[int(ch5[0])], frequencies[int(ch5[-1])], color='blue', alpha=0.5)
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/clean_ch.pdf', transparent=True, bbox_inches='tight')
+plt.show()
+"""
+
+# A plot for SK max and SK min section in SK chapter of thesis
+m = 256
+# pearson type IV PDF produced by sk/sk_thresholds.py script
+p4 = np.load("pdf_M" + str(m) + ".npy")
+sk = np.load("sk_z_M" + str(m) + "_m1_n1_2210_0x.npy")
+fig, ax = plt.subplots()
+ax.semilogy(x, p4)
+ax.hist(sk[600,:], 1000, density = "True",stacked = "True", log = "True")
+plt.show()
+
+"""for m in M:
     p4 = np.load("pdf_M"+str(m)+".npy")
     sk = np.load("sk_z_M" + str(m) + "_m1_n1_2210_0x.npy")
     max_sk = []
@@ -38,7 +90,7 @@ for m in M:
     print("max sk   : ", max(max_sk))
     print("min sk   : ", min(min_sk))
 
-    """#print("median sk:", np.median(max_sk))
+    #print("median sk:", np.median(max_sk))
     print("\n") 
     plt.figure(0)
     #plt.semilogy(x, p4)
