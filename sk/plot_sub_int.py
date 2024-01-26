ba339f4065b0938b597cbfc2bafe79d03165a0df
vereese <vereese@ska.ac.za>
2024-01-26 08:09:18 +0200

    Parameterised input data dir

--- sk/plot_sub_int.py
+++ sk/plot_sub_int.py
@@ -4,6 +4,11 @@ import matplotlib.ticker as ticker
 import sys
 sys.path.append('../')
 from constants import a4_textwidth, a4_textheight, thesis_font
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", dest = "dir", help="Directory where data is located. default: /net/com08/data6/vereese/phd_data/sk_analysis/2210/", default="/net/com08/data6/vereese/phd_data/sk_analysis/2210/")
+args = parser.parse_args()
 
 # Setup fonts and sizes for publication, based on page dimensions in inches
 textwidth = a4_textwidth
@@ -25,40 +30,56 @@ plt.rc('mathtext', fontset='cm')
 plt.rc("text", usetex = True)
 plt.rc("figure", figsize = (textwidth, figheight))
 
-dir = "/home/vereese/data/phd_data/sk_analysis/2210/"
+s_none = np.load(args.dir + "sub_int_intensity_z_2210.npy")
+s_sk = np.load(args.dir + "sub_int_intensity_z_sk_l1siguskmax_M256_2210.npy")
+#s_sk = np.load(args.dir + "sub_int_intensity_z_sk_l4sigu4sig_M2048_2210.npy")
+s_pt = np.load(args.dir + "sub_int_intensity_z_pt_2210.npy")
 
-s_none = np.load(dir + "sub_int_intensity_2210.npy")
-s_sk256 = np.load(dir + "sub_int_intensity_M16384_2210.npy")
-s_pt = np.load(dir + "sub_int_intensity_sigma2210.npy")
+freq_ch = 921 #467 #280 #918 #208 #918
+vmini = np.min(s_sk[:, freq_ch, :])
+vmaxi = np.max(s_sk[:, freq_ch, :])
+
 
 # identified ch 280 as an intermittent RFI ch
 # wrt observation time:
 # 45216 (total # pulses) * J0437_samples_T*time_resolution*10**-6 = 260 
 # 260 / 60 = 4.3
-num_sub_ints = s_sk256.shape[0]
-num_2_roll = int(s_sk256.shape[2]/2 - list(s_sk256[:,280,:].sum(axis=0)).index(max(list(s_sk256[:,280,:].sum(axis=0)))))
-print("roll by: ", num_2_roll)
+num_sub_ints = s_sk.shape[0]
+#num_2_roll = int(s_sk256.shape[2]/2 - list(s_sk256[:, freq_ch, :].sum(axis=0)).index(max(list(s_sk256[:, freq_ch, :].sum(axis=0)))))
+#print("roll by: ", num_2_roll)
 
 plt.figure(0)
-plt.imshow(np.roll(s_sk256[:,280,:], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+#plt.imshow(np.roll(s_sk256[:, freq_ch, :], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+plt.imshow(s_sk[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 func = lambda x, pos: "" if np.isclose(x,0) else x
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
+#plt.plot(s_sk[:, freq_ch, :].sum(axis=0), label="SK")
+#plt.plot(s_none[:, freq_ch, :].sum(axis=0), label="none")
+#plt.plot(s_pt[:, freq_ch, :].sum(axis=0), label="pt")
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
-plt.savefig('/home/vereese/thesis_pics/sub_int_sk_M256_l1siguskmax.pdf', bbox_inches='tight')
+#plt.title("sk")
+plt.savefig('/home/vereese/thesis_pics/sub_int_sk_l1siguskmax_M256_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
+#plt.savefig('/home/vereese/thesis_pics/sub_int_sk_l4sigu4sig_M2048_2210_ch280.pdf', bbox_inches='tight')
+
 
 plt.figure(1)
-plt.imshow(np.roll(s_none[:,280,:], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+#plt.imshow(np.roll(s_none[:, freq_ch, :], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+plt.imshow(s_none[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 func = lambda x, pos: "" if np.isclose(x,0) else x
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
-plt.savefig('/home/vereese/thesis_pics/sub_int_none.pdf', bbox_inches='tight')
+#plt.title("none")
+plt.savefig('/home/vereese/thesis_pics/sub_int_none_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
 
 plt.figure(2)
-plt.imshow(np.roll(s_pt[:,280,:], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+#plt.imshow(np.roll(s_pt[:, freq_ch, :], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
+plt.imshow(s_pt[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 func = lambda x, pos: "" if np.isclose(x,0) else x
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
-plt.savefig('/home/vereese/thesis_pics/sub_int_pt.pdf', bbox_inches='tight')
+#plt.title("pt")
+plt.savefig('/home/vereese/thesis_pics/sub_int_pt_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
+plt.show()
