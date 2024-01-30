de3ffbe603ba0734e12f53369aca31f589b4db86
vereese van tonder <vereese@ska.ac.za>
2024-01-30 13:31:07 +0200

    changed to latex + pdf

--- skew_kurt.py
+++ skew_kurt.py
@@ -3,18 +3,25 @@ from scipy.stats import skewnorm
 import matplotlib.pyplot as plt
 from constants import thesis_font, a4_textwidth, a4_textheight
 
-textwidth = a4_textwidth
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth =  a4_textwidth
 textheight = a4_textheight
 font_size = thesis_font
-plt.rc('font', size=font_size, family='STIXGeneral')
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
 #plt.rc('axes', titlesize=14, labelsize=14)
 plt.rc('axes', titlesize=font_size, labelsize=font_size)
 plt.rc(('xtick', 'ytick'), labelsize=font_size)
 plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
-plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-plt.rc('mathtext', fontset='stix')
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
 
 fig, ax = plt.subplots(1, 1)
 a1 = 4
@@ -30,7 +37,7 @@ ax.set_xlim([x2[0], x1[-1]])
 ax.legend()
 ax.set_ylabel("$f_x(x)$")
 ax.set_xlabel("$x$")
-#plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/skewnessdemo.png', transparent=True, bbox_inches='tight')
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/skewnessdemo.pdf', transparent=True, bbox_inches='tight')
 plt.show()
 
 
