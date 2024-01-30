7d65fa8ce929250d22f1131a020bf5eaade00499
vereese van tonder <vereese@ska.ac.za>
2024-01-30 13:22:34 +0200

    updates to make it latex style and pdf

--- hos/hos_symmetry.py
+++ hos/hos_symmetry.py
@@ -1,5 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
+sys.path.append("../")
+from constants import thesis_font, a4_textwidth, a4_textheight
 
 """
 A script to test symmetry separately 
@@ -8,33 +11,25 @@ References:
 [1] Appendix A.1 "The bispectrum and its relationship to phase-amplitude coupling"
 """
 
-"""font2 = {'family': 'STIXGeneral',
-         'color': 'g',
-         'size': 26,
-         'fontweight': 'bold'
-         }
-
-font = {'family': 'STIXGeneral',
-        'size': 26}
-plt.rc('font', **font)
-"""
-
-font2 = {'family': 'STIXGeneral',
-         'color': 'g',
-         'size': 12,
-         'fontweight': 'bold'
-         }
-textwidth = 9.6 # 128.0 / 25.4 #
-textheight = 7 # 96.0 / 25.4 # 7
-plt.rc('font', size=12, family='STIXGeneral')
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth =  a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
 #plt.rc('axes', titlesize=14, labelsize=14)
-plt.rc('axes', titlesize=12, labelsize=12)
-plt.rc(('xtick', 'ytick'), labelsize=12)
-plt.rc('legend', fontsize=12)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
-plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-plt.rc('mathtext', fontset='stix')
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
 
 def sample(bi, w_1, w_2):
     in1 = w_1 + 500
@@ -80,21 +75,21 @@ for w1 in np.arange(-500, 500):
             write_b(full_bispec, w1, w2, 100000000)
         elif w1 == -0.5 * w2 or w1 == -2 * w2:
             write_b(full_bispec, w1, w2, 100000000)
-plt.figure(0, figsize=[10, 10])
-plt.imshow(np.abs(full_bispec), origin='lower', extent=[-500, 500, -500, 500])
-plt.text(300, 150, '1', fontdict=font2)
-plt.text(150, 300, '2', fontdict=font2)
-plt.text(-100, 400, '3', fontdict=font2)
-plt.text(-300, 350, '4', fontdict=font2)
-plt.text(-400, 250, '5', fontdict=font2)
-plt.text(-400, 50, '6', fontdict=font2)
-plt.text(-300, -150, '7', fontdict=font2)
-plt.text(-150, -300, '8', fontdict=font2)
-plt.text(50, -400, '9', fontdict=font2)
-plt.text(250, -400, '10', fontdict=font2)
-plt.text(350, -300, '11', fontdict=font2)
-plt.text(350, -100, '12', fontdict=font2)
+plt.figure(0)
+plt.imshow(np.abs(full_bispec), aspect='auto', origin='lower', extent=[-500, 500, -500, 500])
+plt.text(300, 150, '1', color='y', weight='bold')
+plt.text(150, 300, '2', color='y', weight='bold')
+plt.text(-100, 400, '3', color='y', weight='bold')
+plt.text(-300, 350, '4', color='y', weight='bold')
+plt.text(-400, 250, '5', color='y', weight='bold')
+plt.text(-400, 50, '6', color='y', weight='bold')
+plt.text(-300, -150, '7', color='y', weight='bold')
+plt.text(-150, -300, '8', color='y', weight='bold')
+plt.text(50, -400, '9', color='y', weight='bold')
+plt.text(250, -400, '10', color='y', weight='bold')
+plt.text(350, -300, '11', color='y', weight='bold')
+plt.text(350, -100, '12', color='y', weight='bold')
 plt.xlabel("$f_1$ [Hz]")
 plt.ylabel("$f_2$ [Hz]")
-plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/sym.png', bbox_inches='tight')
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/sym.pdf', bbox_inches='tight')
 plt.show()
