e91f9176efb80c26ac9adced562e15318ae0200a
vereese <vereese@ska.ac.za>
2024-01-30 14:22:20 +0200

    latex + pdf

--- plot_time_series.py
+++ plot_time_series.py
@@ -1,19 +1,27 @@
 import h5py
 import numpy as np
 from matplotlib import pyplot as plt
-from constants import time_resolution, thesis_font, jai_font, start_indices, time_chunk_size
+from constants import time_resolution, thesis_font, a4_textwidth, a4_textheight, jai_font, start_indices, time_chunk_size
 
-textwidth = 9.6 # inches of page width as per JAI paper for beamer use dimesion: 128.0 / 25.4
-textheight = 7 # inches of page height as per JAI paper for beamer use dimesion: 96.0 / 25.4
-font_size = thesis_font 
-plt.rc('font', size=font_size, family='STIXGeneral')
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth =  a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
+#plt.rc('axes', titlesize=14, labelsize=14)
 plt.rc('axes', titlesize=font_size, labelsize=font_size)
 plt.rc(('xtick', 'ytick'), labelsize=font_size)
 plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
-plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-plt.rc('mathtext', fontset='stix')
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
 
 fn = '1604641234_wide_tied_array_channelised_voltage_0x.h5'
 df = h5py.File('/net/com08/data6/vereese/' + fn, 'r')
@@ -30,6 +38,6 @@ plt.legend()
 plt.xlim([t[0], t[-1]])
 plt.xlabel("observation time [ms]")
 plt.ylabel("real 8 bit data")
-#plt.savefig('/home/vereese/time_series.png', bbox_inches='tight')
+plt.savefig('/home/vereese/thesis_pics/time_series.pdf', transparent=True, bbox_inches='tight')
 
 plt.show()
