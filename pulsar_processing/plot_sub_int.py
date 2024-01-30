a99cd6f7be476e0b30e294f07a38da5ea2a2a173
vereese van tonder <vereese@ska.ac.za>
2024-01-30 14:08:38 +0200

    changed to latex + pdf

--- pulsar_processing/plot_sub_int.py
+++ pulsar_processing/plot_sub_int.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
+sys.path.append("../")
+from constants import thesis_font, a4_textwidth, a4_textheight
 
 # This script assists in calculating the drift in the pulse period
 # It uses eq. 7.5 from the handbook
@@ -9,22 +12,38 @@ from matplotlib import pyplot as plt
 # deltaT in samples = 211375
 # 11.1946499395*(1-(211375/252755968))
 
-font = {'family': 'STIXGeneral',
-        'size': 22}
-plt.rc('font', **font)
+# Setup fonts and sizes for publication, based on page dimensions in inches
+textwidth =  a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
+plt.rc('pdf', fonttype=42)
+#plt.rc('axes', titlesize=14, labelsize=14)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
+plt.rc('lines', markersize=5)
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
+
 DIR = '/home/vereese/git/phd_data/pulsar/'
 PREFIX = 'sub_int_vela_11.'
 files = ['18500936838522.npy', '185031494489326.npy', '185053620637202.npy', '185075.npy', '18512.npy', '1946499395.npy']
-vela_sub_int = np.load(DIR+PREFIX+files[0])
+vela_sub_int = np.load(DIR+PREFIX+files[2])
 last = vela_sub_int[-1,:]
 first = vela_sub_int[8,:] #np.roll(np.asarray(vela_sub_int[8,:]),-700)
 
 plt.figure(0)
-plt.autoscale(True)
-plt.ylabel('observation time [s]')
-plt.xlabel('Pulsar phase')
-plt.imshow(vela_sub_int, aspect='auto', extent=[0,1,0,300], origin='lower')
-
+plt.ylabel('observation time [min]')
+plt.xlabel('pulse phase')
+plt.imshow(vela_sub_int, aspect='auto', extent=[0, 1, 0, 4.3], origin='lower')
+plt.savefig('/home/vereese/Documents/PhD/ThesisTemplate/Figures/subintcor.pdf', transparent=True, bbox_inches='tight')
 
 plt.figure(1)
 plt.plot(last, label='last')
