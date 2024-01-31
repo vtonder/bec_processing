48f2a885b4e1232b34c27394fa42c2eca2b9a927
vereese van tonder <vereese@ska.ac.za>
2024-01-31 21:58:07 +0200

    Had to add ../ to path
    also corrected to pdf and correct latex font

--- sk/kurtosis.py
+++ sk/kurtosis.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
+import sys
+sys.path.append('../')
 from constants import a4_textheight, a4_textwidth, thesis_font
 
 #TODO need to apply normalisation to spectra otherwise the estimator will fail - Nita 2007
--- sk/msk_hist.py
+++ sk/msk_hist.py
@@ -1,21 +1,29 @@
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
 from kurtosis import spectral_kurtosis_cm, s1_s2, ms_spectral_kurtosis_cm
+sys.path.append('../')
 from constants import thesis_font, a4_textwidth, a4_textheight
 
 # Setup fonts and sizes for publication, based on page dimensions in inches
-textwidth =  a4_textwidth
+textwidth = a4_textwidth
 textheight = a4_textheight
 font_size = thesis_font
-plt.rc('font', size=font_size, family='STIXGeneral')
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
-#plt.rc('axes', titlesize=14, labelsize=14)
+# plt.rc('axes', titlesize=14, labelsize=14)
 plt.rc('axes', titlesize=font_size, labelsize=font_size)
 plt.rc(('xtick', 'ytick'), labelsize=font_size)
 plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
-plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-plt.rc('mathtext', fontset='stix')
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.65 * textwidth
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex=True)
+plt.rc("figure", figsize=(textwidth, figheight))
 
 M = 512
 m = 2
@@ -26,8 +34,8 @@ std = 1
 N = 100 * M * FFT_LEN
 
 # These data sets were once off generated using sk_thresholds.py script
-pdf_M512 = np.load("/home/vereese/git/phd_data/sk_analysis/2210/pdf_M512.npy")
-pdf_M2048 = np.load("/home/vereese/git/phd_data/sk_analysis/2210/pdf_M2048.npy")
+pdf_M512 = np.load("/home/vereese/git/phd_data/sk_analysis/pdf_M512.npy")
+pdf_M2048 = np.load("/home/vereese/git/phd_data/sk_analysis/pdf_M2048.npy")
 
 wgn_re = np.random.normal(mean, std, size=N)
 wgn_im = np.random.normal(mean, std, size=N)
@@ -43,10 +51,11 @@ plt.hist(sk.flatten(),1000, density=True, log=True, stacked=True, label="SK hist
 plt.plot(xv, pdf_M512, label="SK PDF M = "+str(M), linewidth=2)
 plt.hist(msk.flatten(),1000, density=True, log=True, stacked=True, label="MSK hist M=512, n=2, m=2")
 plt.plot(xv, pdf_M2048, label="SK PDF M = "+str(M*n*m), linewidth=2)
-plt.legend()
+# loc = 7 <=> center right
+plt.legend(loc=7)
 plt.xlim([0.7,1.3])
 plt.ylim([10**-1,10**1])
 plt.xlabel("SK values")
 plt.ylabel("log(PDF)")
-plt.savefig("/home/vereese/Documents/PhD/ThesisTemplate/Figures/msk_hist")
+plt.savefig("/home/vereese/Documents/PhD/ThesisTemplate/Figures/msk_hist.pdf")
 plt.show()
