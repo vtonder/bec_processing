43ef72fd418c3c725724b4ca59dadb46c41e3c5e
vereese <vereese@ska.ac.za>
2024-02-13 09:30:41 +0200

    Copied fig parameters to the bottom
    and reset jai params for jai paper. This way it prevents pdf's from
    being overwritten

--- sk/plot_sub_int.py
+++ sk/plot_sub_int.py
@@ -11,9 +11,9 @@ parser.add_argument("-d", dest = "dir", help="Directory where data is located. d
 args = parser.parse_args()
 
 # Setup fonts and sizes for publication, based on page dimensions in inches
-textwidth = jai_textwidth # a4_textwidth
-textheight = jai_textheight # a4_textheight
-font_size = jai_font # thesis_font
+textwidth = a4_textwidth
+textheight = a4_textheight
+font_size = thesis_font
 # groups are like plt.figure plt.legend etc
 plt.rc('font', size=font_size, family='serif')
 plt.rc('pdf', fonttype=42)
@@ -24,7 +24,7 @@ plt.rc('legend', fontsize=font_size)
 plt.rc('lines', markersize=5)
 # The following should only be used for beamer
 # plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
-figheight = 0.65 * textwidth
+figheight = 0.65 * textwidth # used for thesis, the first 3 plots 
 plt.rc('mathtext', fontset='cm')
 # to get this working needed to do: sudo apt install cm-super
 plt.rc("text", usetex = True)
@@ -59,7 +59,7 @@ plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
 #plt.title("sk")
-#plt.savefig('/home/vereese/thesis_pics/sub_int_sk_l1siguskmax_M256_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
+plt.savefig('/home/vereese/thesis_pics/sub_int_sk_l1siguskmax_M256_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
 
 plt.figure(1)
 #plt.imshow(np.roll(s_none[:, freq_ch, :], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
@@ -69,7 +69,7 @@ plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
 #plt.title("none")
-#plt.savefig('/home/vereese/thesis_pics/sub_int_none_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
+plt.savefig('/home/vereese/thesis_pics/sub_int_none_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
 
 plt.figure(2)
 #plt.imshow(np.roll(s_pt[:, freq_ch, :], num_2_roll), origin="lower", aspect="auto", extent=[0, 1, 0, 4.3])
@@ -79,14 +79,34 @@ plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
 plt.xlabel("pulsar phase")
 plt.ylabel("observation time [min]")
 #plt.title("pt")
-#plt.savefig('/home/vereese/thesis_pics/sub_int_pt_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
+plt.savefig('/home/vereese/thesis_pics/sub_int_pt_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
 
-ax, fig = plt.subplots(1, 3, sharey=True)
+textwidth = jai_textwidth
+textheight = jai_textheight
+font_size = jai_font
+# groups are like plt.figure plt.legend etc
+plt.rc('font', size=font_size, family='serif')
+plt.rc('pdf', fonttype=42)
+#plt.rc('axes', titlesize=14, labelsize=14)
+plt.rc('axes', titlesize=font_size, labelsize=font_size)
+plt.rc(('xtick', 'ytick'), labelsize=font_size)
+plt.rc('legend', fontsize=font_size)
+plt.rc('lines', markersize=5)
+# The following should only be used for beamer
+# plt.rc('figure', figsize=(0.9 * textwidth, 0.8 * textheight), facecolor='w')
+figheight = 0.3 * textwidth # used for JAI paper, the last plot only 
+plt.rc('mathtext', fontset='cm')
+# to get this working needed to do: sudo apt install cm-super
+plt.rc("text", usetex = True)
+plt.rc("figure", figsize = (textwidth, figheight))
+
+fig, ax = plt.subplots(1, 3, sharey=True)
+fig.tight_layout()
 ax[0].imshow(s_none[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 ax[1].imshow(s_pt[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 ax[2].imshow(s_sk[:, freq_ch, :], origin="lower", aspect="auto", extent=[0, 1, 0, 4.3], vmin=vmini, vmax=vmaxi)
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(func))
-for i in np.arange(2):
+for i in np.arange(3):
     ax[i].set_xlabel("pulsar phase")
 ax[0].set_ylabel("observation time [min]")
 plt.savefig('/home/vereese/jai_pics/sub_int_2210_ch' + str(freq_ch) + '.pdf', bbox_inches='tight')
