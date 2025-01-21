de2dbb34c812ef871cb354e6b1c4652b195431e9
vereese <vereese@ska.ac.za>
2025-01-21 10:17:37 +0200

    changed plot as per examiner recommendation

--- sk/plot_mean_sk.py
+++ sk/plot_mean_sk.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import sys
 sys.path.append('../')
-from constants import frequencies, thesis_font, a4_textwidth, a4_textheight
+from constants import frequencies, thesis_font, a4_textwidth, a4_textheight, lower_limit_3s, upper_limit_3s
 import argparse
 
 # A script to plot the mean SK across the observation for J0437-4715
@@ -28,7 +28,7 @@ plt.rc("text", usetex = True)
 plt.rc("figure", figsize = (textwidth, figheight))
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", dest = "dir", help = "directory where data is located. default location: /home/vereese/data/phd_data/sk_analysis/2210/", default = "/home/vereese/data/phd_data/sk_analysis/2210/")
+parser.add_argument("-d", dest = "dir", help = "directory where data is located. default location: /home/vereese/git/phd_data/sk_analysis/2210/", default = "/home/vereese/git/phd_data/sk_analysis/2210/")
 
 args = parser.parse_args()
 DIR = args.dir
@@ -48,17 +48,27 @@ m_skx_M2048 = skx_M2048.mean(axis = 1)
 m_sky_M2048 = sky_M2048.mean(axis = 1)
 
 fig, ax = plt.subplots()
-ax.plot(frequencies, m_sky_M2048, label="V-pol, $M$ = 2048")
-ax.plot(frequencies, m_skx_M2048, label="H-pol, $M$ = 2048")
-ax.plot(frequencies, m_sky_M256, label="V-pol, $M$ = 256")
-ax.plot(frequencies, m_skx_M256, label="H-pol, $M$ = 256")
+ax.plot(frequencies, m_sky_M2048, label="V-pol, $M$ = 2048", linewidth=2)
+ax.plot(frequencies, m_skx_M2048, label="H-pol, $M$ = 2048", linewidth=2)
+ax.plot(frequencies, m_sky_M256, label="V-pol, $M$ = 256", linewidth=2)
+ax.plot(frequencies, m_skx_M256, label="H-pol, $M$ = 256", linewidth=2)
+#ax.semilogy(frequencies, m_sky_M2048, label="V-pol, $M$ = 2048", linewidth=2)
+#ax.semilogy(frequencies, m_skx_M2048, label="H-pol, $M$ = 2048", linewidth=2)
+#ax.semilogy(frequencies, m_sky_M256, label="V-pol, $M$ = 256", linewidth=2)
+#ax.semilogy(frequencies, m_skx_M256, label="H-pol, $M$ = 256", linewidth=2)
+#ax.hlines(lower_limit_3s[256], frequencies[0], frequencies[-1], linestyle = "--", color="red", label="$\pm3\sigma$ thresholds, M=256")
+#ax.hlines(upper_limit_3s[256], frequencies[0], frequencies[-1], linestyle = "--", color="red")
+#ax.hlines(lower_limit_3s[2048], frequencies[0], frequencies[-1], linestyle = "--", color="blue", label="$\pm3\sigma$ thresholds, M=2048")
+#ax.hlines(upper_limit_3s[2048], frequencies[0], frequencies[-1], linestyle = "--", color="blue")
+ax.hlines(1, frequencies[0], frequencies[-1], linestyle='--', color="black", label="$\overline{SK} = 1$") #, linewidth=2)
 #ax.plot(frequencies, m_sky_M64, label="V-pol, M = 64")
 #ax.plot(frequencies, m_skx_M64, label="H-pol, M = 64")
 ax.set_xlabel("frequency [MHz]")
 ax.set_ylabel("$\overline{SK}$")
 ax.set_xlim([frequencies[0], frequencies[-1]])
 ax.set_ylim([0.995, 1.015])
+#ax.set_ylim([0.6, 1.55])
 plt.legend()
-plt.savefig('/home/vereese/thesis_pics/mean_sk.pdf', transparent=True, bbox_inches='tight')
+plt.savefig('/home/vereese/thesis_pics/mean_sk1.pdf', transparent=True, bbox_inches='tight')
 plt.show()
 
