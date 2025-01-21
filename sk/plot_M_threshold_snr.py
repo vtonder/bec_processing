f9d79fd8c0ac54bc6bc33ab6f9b695e43fabd5d3
vereese <vereese@ska.ac.za>
2025-01-21 10:15:34 +0200

    plot vela's optimisation

--- sk/plot_M_threshold_snr.py
+++ sk/plot_M_threshold_snr.py
@@ -29,8 +29,12 @@ plt.rc('mathtext', fontset='cm')
 plt.rc("text", usetex = True)
 plt.rc("figure", figsize = (textwidth, figheight))
 
-lt = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig", "skmin"] # lt : lower threshold
-ut = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig", "skmax"] # ut : upper threshold
+#lt = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig", "skmin"] # lt : lower threshold
+#ut = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig", "skmax"] # ut : upper threshold
+
+lt = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig"] # lt : lower threshold
+ut = ["0sig", "0_5sig","1sig", "2sig", "2_5sig", "3sig", "4sig"] # ut : upper threshold
+
 
 M = ["64", "128", "256", "512", "1024", "2048", "4096", "8192"]
 
@@ -43,18 +47,16 @@ for i in np.arange(len(M)):
 
     for j in np.arange(len(lt)):
         # set initialise to False and call compute - this way no mask is applied
-
-        print("lt: ", lt[j])
         print("")
-
-        intensity_l = PI(args.dir, "sk_intensity_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", "sk_num_nz_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy")
+        print("lt: ", lt[j])
+        intensity_l = PI(args.dir, "sk_ypol_intensity_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_1064_p144.npy", "sk_ypol_num_nz_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_1064_p144.npy")
         #intensity_l = PI(args.dir, "sk_intensity_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", "sk_num_nz_z_l" + lt[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", initialise = False)
         #intensity_l.compute()
 
-        print("ut: ", ut[j])
         print("")
-
-        intensity_u = PI(args.dir, "sk_intensity_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", "sk_num_nz_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy")
+        print("ut: ", ut[j])
+        intensity_u = PI(args.dir, "sk_ypol_intensity_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_1064_p144.npy", "sk_ypol_num_nz_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_1064_p144.npy")
+        #intensity_u = PI(args.dir, "sk_intensity_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", "sk_num_nz_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy")
         #intensity_u = PI(args.dir, "sk_intensity_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", "sk_num_nz_z_u" + ut[j] + "_M" + M[i] + "_m1_n1_2210_p45216.npy", initialise = False)
         #intensity_u.compute()
 
@@ -64,29 +66,29 @@ for i in np.arange(len(M)):
 vmini = np.min(ut_snr) 
 vmaxi = np.max(lt_snr) 
 
-lt_labels = ("$0\sigma$", "$0.5\sigma$", "$1\sigma$", "$2\sigma$", "$2.5\sigma$", "$3\sigma$", "$4\sigma$", "$SK_{min}$")
+lt_labels = ("$0\sigma$", "$0.5\sigma$", "$1\sigma$", "$2\sigma$", "$2.5\sigma$", "$3\sigma$", "$4\sigma$") #, "$SK_{min}$")
 x_range = np.arange(len(lt_labels))
 y_range = np.arange(len(M))
-fig, ax = plt.subplots()
-snr_im_lt = ax.imshow(lt_snr, origin="lower", aspect="auto", vmin=vmini, vmax=vmaxi)
-cbar = fig.colorbar(snr_im_lt, ax=ax)
-cbar.minorticks_on()
-ax.set_ylabel("$M$")
-ax.set_xlabel("Lower threshold")
-ax.set_xticks(x_range, lt_labels)
-ax.set_yticks(y_range, M)
-plt.savefig('/home/vereese/thesis_pics/lower_threshold_masked.pdf', transparent=True, bbox_inches='tight')
+#fig, ax = plt.subplots()
+#snr_im_lt = ax.imshow(lt_snr, origin="lower", aspect="auto", vmin=vmini, vmax=vmaxi)
+#cbar = fig.colorbar(snr_im_lt, ax=ax)
+#cbar.minorticks_on()
+#ax.set_ylabel("$M$")
+#ax.set_xlabel("Lower threshold")
+#ax.set_xticks(x_range, lt_labels)
+#ax.set_yticks(y_range, M)
+#plt.savefig('/home/vereese/thesis_pics/lower_threshold_masked.pdf', transparent=True, bbox_inches='tight')
 
-ut_labels = ("$0\sigma$", "$0.5\sigma$", "$1\sigma$", "$2\sigma$", "$2.5\sigma$", "$3\sigma$", "$4\sigma$", "$SK_{max}$")
-fig1, ax1 = plt.subplots()
-snr_im_ut = ax1.imshow(ut_snr, origin="lower", aspect="auto", vmin=vmini, vmax=vmaxi)
-cbar1 = fig1.colorbar(snr_im_ut, ax=ax1)
-cbar1.minorticks_on()
-ax1.set_xticks(x_range, ut_labels)
-ax1.set_yticks(y_range, M)
-ax1.set_ylabel("$M$")
-ax1.set_xlabel("Upper threshold")
-plt.savefig('/home/vereese/thesis_pics/upper_threshold_masked.pdf', transparent=True, bbox_inches='tight')
+ut_labels = ("$0\sigma$", "$0.5\sigma$", "$1\sigma$", "$2\sigma$", "$2.5\sigma$", "$3\sigma$", "$4\sigma$") #, "$SK_{max}$")
+#fig1, ax1 = plt.subplots()
+#snr_im_ut = ax1.imshow(ut_snr, origin="lower", aspect="auto", vmin=vmini, vmax=vmaxi)
+#cbar1 = fig1.colorbar(snr_im_ut, ax=ax1)
+#cbar1.minorticks_on()
+#ax1.set_xticks(x_range, ut_labels)
+#ax1.set_yticks(y_range, M)
+#ax1.set_ylabel("$M$")
+#ax1.set_xlabel("Upper threshold")
+#plt.savefig('/home/vereese/thesis_pics/upper_threshold_masked.pdf', transparent=True, bbox_inches='tight')
 
 # Plot for JAI paper therefore reset some params
 textwidth = jai_textwidth
@@ -113,6 +115,6 @@ ax2[1].set_xlabel("Upper threshold")
 fig2.subplots_adjust()
 cbar2 = fig2.colorbar(snr_im_ltut, ax=ax2)
 cbar2.minorticks_on()
-plt.savefig('/home/vereese/jai_pics/lower_upper_threshold_masked.pdf', transparent=True, bbox_inches='tight')
+#plt.savefig('/home/vereese/jai_pics/lower_upper_threshold_masked_vela.pdf', transparent=True, bbox_inches='tight')
 
 plt.show()
